9eea3ffb8738d02118d8edfb2f3682b0f33d35fe
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-28 21:32:14 +0800

    Implement dark mode to image

--- src/screens/ChooseRole/index.jsx
+++ src/screens/ChooseRole/index.jsx
@@ -1,51 +1,23 @@
 import React from "react";
 import salonLogo from "@assets/salon-logo.png";
-import { Image, View, SafeAreaView, Text, TouchableOpacity } from "react-native";
+import salonLogoWhite from "@assets/salon-logo-white.png";
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { changeColor } from "@utils";
 
 export default function () {
   const { backgroundColor, textColor } = changeColor();
   return (
     <>
-    <SafeAreaView style={{backgroundColor}} className="bg-primary-accent flex-1 flex-column">
-        <View className="flex-1 justify-end items-center">
-            <Image
-                source={salonLogo}
-                // className={`w-100 h-100`}
-                resizeMode="cover"
-            />
-        </View>
-        <View className="flex-1 justify-start items-center">
-            <Text  style={{ color: textColor }} className={`text-2xl font-base mt-6 mb-2`}>
-                Welcome to
-            </Text>
-            <Text  style={{ color: textColor }} className={`text-[28px] font-semibold`}>
-                Lanlee Beauty Lounge
-            </Text>
-        <TouchableOpacity className={`flex`}>
-              <View className={`mt-10 w-full`}>
-                <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
-                  <Text
-                    className={`text-neutral-light font-semibold text-center text-lg`}
-                    style={{ color: textColor }}
-                  >
-                    LogIn
-                  </Text>
-                </View>
-              </View>
-              <View className={`mt-10 w-full`}>
-                <View className={`px-[42px] py-2 rounded-3xl bg-primary-accent`}>
-                  <Text
-                    className={`text-neutral-light font-semibold text-center text-lg`}
-                    style={{ color: textColor }}
-                  >
-                    SignUp
-                  </Text>
-                </View>
-              </View>
-        </TouchableOpacity>
-        </View>
-    </SafeAreaView>
+      <SafeAreaView
+        style={{ backgroundColor }}
+        className="flex-1 bg-primary-accent flex-column"
+      ></SafeAreaView>
     </>
   );
 }
--- src/screens/UserPick/index.jsx
+++ src/screens/UserPick/index.jsx
@@ -1,51 +1,61 @@
 import React from "react";
 import salonLogo from "@assets/salon-logo.png";
-import { Image, View, SafeAreaView, Text, TouchableOpacity } from "react-native";
+import salonLogoWhite from "@assets/salon-logo-white.png";
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { changeColor } from "@utils";
 
 export default function () {
-  const { backgroundColor, textColor } = changeColor();
+  const { backgroundColor, textColor, colorScheme } = changeColor();
+  const imageSource = colorScheme === "dark" ? salonLogoWhite : salonLogo;
   return (
     <>
-    <SafeAreaView style={{backgroundColor}} className="bg-primary-accent flex-1 flex-column">
-        <View className="flex-1 justify-end items-center">
-            <Image
-                source={salonLogo}
-                // className={`w-100 h-100`}
-                resizeMode="cover"
-            />
+      <SafeAreaView style={{ backgroundColor }} className="flex-1 flex-column">
+        <View className="items-center justify-end flex-1">
+          <Image source={imageSource} resizeMode="cover" />
         </View>
-        <View className="flex-1 justify-start items-center">
-            <Text  style={{ color: textColor }} className={`text-2xl font-base mt-6 mb-2`}>
-                Welcome to
-            </Text>
-            <Text  style={{ color: textColor }} className={`text-[28px] font-semibold`}>
-                Lanlee Beauty Lounge
-            </Text>
-        <TouchableOpacity className={`flex`}>
-              <View className={`mt-10 w-full`}>
-                <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
-                  <Text
-                    className={`text-neutral-light font-semibold text-center text-lg`}
-                    style={{ color: textColor }}
-                  >
-                    LogIn
-                  </Text>
-                </View>
+        <View className="items-center justify-start flex-1">
+          <Text
+            style={{ color: textColor }}
+            className={`text-2xl font-base mt-6 mb-2`}
+          >
+            Welcome to
+          </Text>
+          <Text
+            style={{ color: textColor }}
+            className={`text-[28px] font-semibold`}
+          >
+            Lanlee Beauty Lounge
+          </Text>
+          <TouchableOpacity className={`flex`}>
+            <View className={`mt-10 w-full`}>
+              <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
+                <Text
+                  className={`text-neutral-light font-semibold text-center text-lg`}
+                  style={{ color: textColor }}
+                >
+                  LogIn
+                </Text>
               </View>
-              <View className={`mt-10 w-full`}>
-                <View className={`px-[42px] py-2 rounded-3xl bg-primary-accent`}>
-                  <Text
-                    className={`text-neutral-light font-semibold text-center text-lg`}
-                    style={{ color: textColor }}
-                  >
-                    SignUp
-                  </Text>
-                </View>
+            </View>
+            <View className={`mt-10 w-full`}>
+              <View className={`px-[42px] py-2 rounded-3xl bg-primary-accent`}>
+                <Text
+                  className={`text-neutral-light font-semibold text-center text-lg`}
+                  style={{ color: textColor }}
+                >
+                  SignUp
+                </Text>
               </View>
-        </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
         </View>
-    </SafeAreaView>
+      </SafeAreaView>
     </>
   );
 }
