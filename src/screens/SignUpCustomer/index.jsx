6452d68f6b36851298f2d158cc487d962b2ce9d7
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-30 17:54:16 +0800

    Add dimensionLayout for landscape view

--- src/screens/SignUpCustomer/index.jsx
+++ src/screens/SignUpCustomer/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Signup } from "@components";
 import { useNavigation } from "@react-navigation/native";
+import { dimensionLayout } from "@utils";
 
 export default function () {
   const navigation = useNavigation();
+  const isDimensionLayout = dimensionLayout();
   const initialState = {
     userName: "",
     email: "",
@@ -21,6 +23,8 @@ export default function () {
         footerTitle={`Already have an account?`}
         footerLinkTitle={`Sign in`}
         navigateBack={() => navigation.goBack()}
+        navigateTo={() => navigation.navigate("Login")}
+        dimensionLayout={isDimensionLayout}
       />
     </>
   );
