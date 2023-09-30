d935720dbcc93bc42ff8c6b13f2ec8503d290301
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-30 20:00:44 +0800

    Add self closing tag

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -97,98 +97,103 @@ export default function MyComponent({
   }, []);
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView style={{ backgroundColor }} className={`relative flex-1`}>
-        <BackIcon navigateBack={navigateBack} textColor={textColor} />
-        <View
-          className={`justify-start ${
-            dimensionLayout ? "flex-col items-center" : "flex-row items-start"
-          }`}
-        >
-          <Image
-            source={imageSource}
-            className={`${
-              dimensionLayout ? "w-[60%] h-[60%]" : "ml-10 w-[40%] h-[55%]"
+    <>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SafeAreaView style={{ backgroundColor }} className={`relative flex-1`}>
+          <BackIcon navigateBack={navigateBack} textColor={textColor} />
+          <View
+            className={`justify-start ${
+              dimensionLayout ? "flex-col items-center" : "flex-row items-start"
             }`}
-            resizeMode="contain"
-          />
-          <View className={`flex-1 items-center justify-start`}>
-            <Text
-              style={{ color: formData.textColor }}
-              className={`font-semibold ${
-                dimensionLayout ? "my-2 text-3xl" : "my-1 text-xl"
+          >
+            <Image
+              source={imageSource}
+              className={`${
+                dimensionLayout ? "w-[60%] h-[60%]" : "ml-10 w-[40%] h-[55%]"
               }`}
-            >
-              {title}
-            </Text>
-            <Text
-              style={{ color: formData.textColor }}
-              className={`mb-2 text-sm font-base`}
-            >
-              {description}
-            </Text>
-            <KeyboardAvoidingView
-              behavior="padding"
-              className={`${dimensionLayout ? "w-[300px]" : "w-[375px]"}`}
-            >
-              <ScrollView style={{ height: scrollViewHeight }}>
-                {Object.keys(initialState).map((field) => (
-                  <TextInput
-                    key={field}
-                    style={{ color: formData.textColor }}
-                    className={`border-b ${
-                      dimensionLayout ? "mb-6" : "mb-3"
-                    } ${borderColor}`}
-                    placeholder={`Enter your ${field}`}
-                    placeholderTextColor={formData.placeholderColor}
-                    autoCapitalize="none"
-                    onFocus={handleTextInputFocus}
-                    onBlur={handleTextInputBlur}
-                    onChangeText={(text) => handleInputChange(field, text)}
-                    value={formData[field]}
-                    secureTextEntry={field === "password"}
-                    keyboardType={
-                      field === "contactNumber" ? "numeric" : "default"
-                    }
-                  />
-                ))}
-                <View
-                  className={`items-center justify-start ${
-                    dimensionLayout ? "flex-col" : "flex-row gap-x-2"
-                  }`}
-                >
-                  <TouchableOpacity onPress={navigateTo}>
-                    <View className={`w-full mb-2`}>
-                      <View
-                        className={`py-2 px-6 rounded-lg bg-primary-accent ${
-                          dimensionLayout ? "flex-col" : "flex-row gap-x-2"
-                        }`}
-                      >
-                        <Text
-                          className={`text-neutral-light font-semibold text-center text-lg`}
-                          style={{ color: textColor }}
+              resizeMode="contain"
+            />
+            <View className={`flex-1 items-center justify-start`}>
+              <Text
+                style={{ color: formData.textColor }}
+                className={`font-semibold ${
+                  dimensionLayout ? "my-2 text-3xl" : "my-1 text-xl"
+                }`}
+              >
+                {title}
+              </Text>
+              <Text
+                style={{ color: formData.textColor }}
+                className={`mb-2 text-sm font-base`}
+              >
+                {description}
+              </Text>
+              <KeyboardAvoidingView
+                behavior="padding"
+                className={`${dimensionLayout ? "w-[300px]" : "w-[375px]"}`}
+              >
+                <ScrollView style={{ height: scrollViewHeight }}>
+                  {Object.keys(initialState).map((field) => (
+                    <TextInput
+                      key={field}
+                      style={{ color: formData.textColor }}
+                      className={`border-b ${
+                        dimensionLayout ? "mb-6" : "mb-3"
+                      } ${borderColor}`}
+                      placeholder={`Enter your ${field}`}
+                      placeholderTextColor={formData.placeholderColor}
+                      autoCapitalize="none"
+                      onFocus={handleTextInputFocus}
+                      onBlur={handleTextInputBlur}
+                      onChangeText={(text) => handleInputChange(field, text)}
+                      value={formData[field]}
+                      secureTextEntry={field === "password"}
+                      keyboardType={
+                        field === "contactNumber" ? "numeric" : "default"
+                      }
+                    />
+                  ))}
+                  <View
+                    className={`items-center justify-start ${
+                      dimensionLayout ? "flex-col" : "flex-row gap-x-2"
+                    }`}
+                  >
+                    <TouchableOpacity onPress={navigateTo}>
+                      <View className={`w-full mb-2`}>
+                        <View
+                          className={`py-2 px-6 rounded-lg bg-primary-accent ${
+                            dimensionLayout ? "flex-col" : "flex-row gap-x-2"
+                          }`}
                         >
-                          {buttonTitle}
-                        </Text>
+                          <Text
+                            className={`text-neutral-light font-semibold text-center text-lg`}
+                            style={{ color: textColor }}
+                          >
+                            {buttonTitle}
+                          </Text>
+                        </View>
                       </View>
-                    </View>
-                  </TouchableOpacity>
-                  <View className={`gap-x-2 flex-row`}>
-                    <Text style={{ color: textColor }} className={`text-base`}>
-                      {footerTitle}
-                    </Text>
-                    <TouchableOpacity>
-                      <Text className={`text-primary-accent text-base`}>
-                        {footerLinkTitle}
-                      </Text>
                     </TouchableOpacity>
+                    <View className={`gap-x-2 flex-row`}>
+                      <Text
+                        style={{ color: textColor }}
+                        className={`text-base`}
+                      >
+                        {footerTitle}
+                      </Text>
+                      <TouchableOpacity>
+                        <Text className={`text-primary-accent text-base`}>
+                          {footerLinkTitle}
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
                   </View>
-                </View>
-              </ScrollView>
-            </KeyboardAvoidingView>
+                </ScrollView>
+              </KeyboardAvoidingView>
+            </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </>
   );
 }
