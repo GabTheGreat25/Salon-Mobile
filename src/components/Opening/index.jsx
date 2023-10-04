f88d0bbc2e21dee5ba01b43bef9fc8e927d0b942
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-10-04 10:58:45 +0800

    Refactor Opening

--- src/components/Opening/index.jsx
+++ src/components/Opening/index.jsx
@@ -9,7 +9,6 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { changeColor } from "@utils";
-import { Feather } from "@expo/vector-icons";
 import { BackIcon } from "@helpers";
 
 export default function ({
@@ -36,64 +35,74 @@ export default function ({
       >
         <BackIcon navigateBack={navigateBack} textColor={textColor} />
         <View
-          className={`flex-1 ${
-            dimensionLayout
-              ? "justify-end items-center"
-              : "justify-start items-end"
+          className={`flex-1 justify-center items-center ${
+            dimensionLayout ? "flex-col" : "flex-row"
           }`}
         >
-          <Image
-            source={imageSource}
-            className={`flex-1 w-full h-full ${
-              dimensionLayout ? "mb-0" : "mb-5"
+          <View
+            className={`flex-1 ${
+              dimensionLayout
+                ? "justify-end items-center"
+                : "justify-start items-end"
             }`}
-            resizeMode="contain"
-          />
-        </View>
-        <View
-          className={`items-center ${
-            dimensionLayout ? "justify-start" : "justify-center"
-          } flex-1`}
-        >
-          {showTitle && (
-            <Text
-              style={{ color: textColor }}
-              className={`font-base mb-2 text-2xl ${
-                dimensionLayout ? "mt-6" : "mt-0"
-              }`}
-            >
-              {title}
-            </Text>
-          )}
-          {showName && (
-            <Text
-              style={{ color: textColor }}
-              className={`font-semibold text-[28px]`}
-            >
-              {name}
-            </Text>
-          )}
-          <View className={`flex`}>
-            <TouchableOpacity className={`mt-5`} onPress={navigateFirstButton}>
-              <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
-                <Text
-                  className={`text-neutral-light font-semibold text-center text-lg`}
-                  style={{ color: textColor }}
-                >
-                  {firstButton}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity className={`mt-8`} onPress={navigateSecondButton}>
-              <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
-                <Text
-                  className={`text-neutral-light font-semibold text-center text-lg`}
-                  style={{ color: textColor }}
-                >
-                  {secondButton}
-                </Text>
-              </View>
-            </TouchableOpacity>
+          >
+            <Image
+              source={imageSource}
+              className={`${dimensionLayout ? "mb-0" : "mb-5"}`}
+              resizeMode="contain"
+            />
+          </View>
+          <View
+            className={`items-center ${
+              dimensionLayout ? "justify-start" : "justify-center"
+            } flex-1`}
+          >
+            {showTitle && (
+              <Text
+                style={{ color: textColor }}
+                className={`font-base mb-2 text-2xl ${
+                  dimensionLayout ? "mt-6" : "mt-0"
+                }`}
+              >
+                {title}
+              </Text>
+            )}
+            {showName && (
+              <Text
+                style={{ color: textColor }}
+                className={`font-semibold text-[28px]`}
+              >
+                {name}
+              </Text>
+            )}
+            <View>
+              <TouchableOpacity
+                className={`mt-5`}
+                onPress={navigateFirstButton}
+              >
+                <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
+                  <Text
+                    className={`text-neutral-light font-semibold text-center text-lg`}
+                    style={{ color: textColor }}
+                  >
+                    {firstButton}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className={`mt-8`}
+                onPress={navigateSecondButton}
+              >
+                <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
+                  <Text
+                    className={`text-neutral-light font-semibold text-center text-lg`}
+                    style={{ color: textColor }}
+                  >
+                    {secondButton}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </SafeAreaView>
