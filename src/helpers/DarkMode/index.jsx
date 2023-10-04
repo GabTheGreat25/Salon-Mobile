97fefad0413f7e3675fc7c95f57128d2c4b23b1b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-10-04 22:16:35 +0800

    Add empty fragments

--- src/helpers/DarkMode/index.jsx
+++ src/helpers/DarkMode/index.jsx
@@ -5,13 +5,15 @@ import { RESOURCE } from "@constants";
 
 export default function ({ name, color, toggle }) {
   return (
-    <TouchableOpacity
-      className="absolute z-[1000] right-5 top-5"
-      onPress={toggle}
-    >
-      <Text selectable={false}>
-        <Feather name={name} size={RESOURCE.NUMBER.FORTY} color={color} />
-      </Text>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        className="absolute z-[1000] right-5 top-5"
+        onPress={toggle}
+      >
+        <Text selectable={false}>
+          <Feather name={name} size={RESOURCE.NUMBER.FORTY} color={color} />
+        </Text>
+      </TouchableOpacity>
+    </>
   );
 }
