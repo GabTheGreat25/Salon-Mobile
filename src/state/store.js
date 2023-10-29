e0a65022ea299f1aaf5ae48051eef481cbff8de9
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-29 17:51:54 +0800

    Add store state

---
+++ src/state/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api/reducer";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { rootReducer } from "./reducer";
+import { RESOURCE } from "../constants";
+import { API_URL } from "../env";
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+  devTools: API_URL !== RESOURCE.PRODUCTION,
+});
+setupListeners(store.dispatch);
