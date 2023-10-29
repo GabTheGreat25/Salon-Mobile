110ec37e64605c924ad8ce3b7af2075bed1b41ea
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-29 17:49:34 +0800

    Add api constant

---
+++ src/constants/api.js
@@ -0,0 +1,12 @@
+import TAGS from "./tags";
+
+export default {
+    GET: "GET",
+    POST: "POST",
+    PATCH: "PATCH",
+    PUT: "PUT",
+    DELETE: "DELETE",
+    TAGS: [
+        TAGS.TESTS,
+    ],
+};
