954fb87316b83e489db7f6844353ad5e83058567
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-31 23:13:13 +0800

    Refactor store

--- src/state/store.js
+++ src/state/store.js
@@ -1,14 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { api } from "./api/reducer";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { rootReducer } from "./reducer";
-import { RESOURCE } from "../constants";
-import { API_URL } from "../env";
+import {
+  configureStore
+} from "@reduxjs/toolkit";
+import {
+  api
+} from "./api/reducer";
+import {
+  setupListeners
+} from "@reduxjs/toolkit/query";
+import {
+  rootReducer
+} from "./reducer";
+import {
+  RESOURCE
+} from "../constants";
+import {
+  API_URL
+} from "../env";
 
 export const store = configureStore({
-  reducer: {
-    [api.reducerPath]: api.reducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
   devTools: API_URL !== RESOURCE.PRODUCTION,
