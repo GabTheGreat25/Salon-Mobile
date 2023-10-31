2b63cea618f160c2f01cf861895dd6619a137799
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-31 23:14:02 +0800

    Add authReducer logic

---
+++ src/state/auth/authReducer.js
@@ -0,0 +1,45 @@
+import {
+  createSlice
+} from "@reduxjs/toolkit";
+import {
+  api
+} from "../api/reducer";
+import {
+  initialState
+} from "./state";
+import {
+  TAGS
+} from "../../constants";
+
+export const authSlice = createSlice({
+  name: TAGS.AUTH,
+  initialState,
+  reducers: {
+    logout(state) {
+      state.token = "";
+      state.user = {};
+      state.authenticated = false;
+      state.loggedInUserId = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      api.endpoints.login.matchFulfilled,
+      (state, {
+        payload
+      }) => {
+        if (payload?.success === true) {
+          state.token = payload?.details?.accessToken;
+          state.user = payload?.details?.user;
+          state.authenticated = true;
+          state.loggedInUserId = state?.user?._id;
+        }
+      }
+    );
+  },
+});
+
+export const {
+  logout
+} = authSlice.actions;
+export default authSlice.reducer;
