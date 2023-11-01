40c65db4e08af908128c4cf986d8a291aa84cda6
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-01 21:53:30 +0800

    Add persist in app js

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { store } from "./src/state/store";
+import { store, persistor } from "./src/state/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import Main from "./Main";
 
 export default function () {
   return (
     <>
-    <Provider store={store}>
-      <Main />
-    </Provider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Main />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
