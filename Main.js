40f4de291bd73b88160f3912b6944a8fe36fe72c
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-03 14:11:37 +0800

    Add authentication and user roles logic for route control

--- Main.js
+++ Main.js
@@ -1,8 +1,8 @@
-
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useSelector } from "react-redux";
 import {
   Home,
   BecomeCustomer,
@@ -32,6 +32,9 @@ export default function () {
   const icon = colorScheme === "dark" ? "sun" : "moon";
   const barStyle = colorScheme === "dark" ? "light-content" : "dark-content";
 
+  const authenticated = useSelector((state) => state.auth?.authenticated);
+  const userRoles = useSelector((state) => state.auth?.user?.roles)
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
@@ -48,27 +51,55 @@ export default function () {
         <>
           <StatusBar barStyle={barStyle} backgroundColor={backgroundColor} />
           <NavigationContainer>
-            <Stack.Navigator
-              initialRouteName="LoginUser"
-              screenOptions={{
-                headerShown: false,
-              }}
-            >
-              <Stack.Screen name="Test" component={Test} />
-              <Stack.Screen name="TestGetById" component={TestGetById} />
-              <Stack.Screen name="CreateTest" component={CreateTest} />
-              <Stack.Screen name="EditTest" component={EditTest} />
-              <Stack.Screen name="Home" component={Home} />
-              <Stack.Screen name="BecomeEmployee" component={BecomeEmployee} />
-              <Stack.Screen name="BecomeCustomer" component={BecomeCustomer} />
-              <Stack.Screen name="UserPick" component={UserPick} />
-              <Stack.Screen name="ChooseRole" component={ChooseRole} />
-              <Stack.Screen name="LoginUser" component={LoginUser} />
-              <Stack.Screen name="SignUpCustomer" component={SignUpCustomer} />
-              <Stack.Screen name="SignUpEmployee" component={SignUpEmployee} />
-              <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
-              <Stack.Screen name="Drawer" component={Drawer} />
-            </Stack.Navigator>
+            {authenticated ? (
+              userRoles.includes("Customer") ? (
+                <Stack.Navigator
+                  initialRouteName="Drawer"
+                  screenOptions={{
+                    headerShown: false,
+                  }}
+                >
+                  <Stack.Screen name="Drawer" component={Drawer} />
+                </Stack.Navigator>
+              ) : userRoles.includes("Employee") ? (
+                <Stack.Navigator
+                  initialRouteName="Home"
+                  screenOptions={{
+                    headerShown: false,
+                  }}
+                >
+                <Stack.Screen name="Home" component={Home} />
+                </Stack.Navigator>
+              ) : (
+                <Stack.Navigator
+                  initialRouteName="Test"
+                  screenOptions={{
+                    headerShown: false,
+                  }}
+                >
+                <Stack.Screen name="Test" component={Test} />
+                <Stack.Screen name="TestGetById" component={TestGetById} />
+                <Stack.Screen name="CreateTest" component={CreateTest} />
+                <Stack.Screen name="EditTest" component={EditTest} />
+                </Stack.Navigator>
+              )
+            ) : (
+              <Stack.Navigator
+                initialRouteName="LoginUser"
+                screenOptions={{
+                  headerShown: false,
+                }}
+              >
+                <Stack.Screen name="LoginUser" component={LoginUser} />
+                <Stack.Screen name="SignUpCustomer" component={SignUpCustomer} />
+                <Stack.Screen name="SignUpEmployee" component={SignUpEmployee} />
+                <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
+                <Stack.Screen name="ChooseRole" component={ChooseRole} />
+                <Stack.Screen name="UserPick" component={UserPick} />
+                <Stack.Screen name="BecomeCustomer" component={BecomeCustomer} />
+                <Stack.Screen name="BecomeEmployee" component={BecomeEmployee} />
+              </Stack.Navigator>
+            )}
           </NavigationContainer>
           <DarkMode toggle={toggleColorScheme} name={icon} color={textColor} />
         </>
