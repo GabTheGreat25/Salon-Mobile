488b16e638dc88f8f772fbb76b7ea3ef232a9479
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-05 23:25:33 +0800

    Add createCustomer validation

---
+++ src/validation/createCustomer.js
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+export default yup.object({
+    name: yup.string("Enter your Name").required("Name is required").min(2, "Your name should be of minimum 2 characters legth").max(30, "Your name cannot exceed 30 characters"),
+    email: yup.string("Enter your email").email("Enter a valid email").required("Email is required"),
+    password: yup.string("Enter your password").required("Password is required").min(8, "Password should be of minimum 8 characters length"),
+    contact_number: yup.string("Enter your contact number").required("Contact number is required").min(11, "Please enter a valid 11-digit phone number"),
+});
