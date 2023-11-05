0d20f65a7c69d9fb7f3b0b0344b2bd4b3442190f
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-05 23:25:43 +0800

    Add loginUser validation

---
+++ src/validation/loginUser.js
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+export default yup.object({
+    email: yup.string("Enter your email").email("Enter a valid email").required("Email is required"),
+    password: yup
+        .string("Enter your password")
+        .required("Password is required")
+        .min(8, "Password should be of minimum 8 characters length"),
+});
