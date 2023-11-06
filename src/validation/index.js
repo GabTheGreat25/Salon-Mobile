0629c0d3c2538e8172427c3a911be06f27220415
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-07 00:19:56 +0800

    Add createEmployee validation

--- src/validation/index.js
+++ src/validation/index.js
@@ -2,10 +2,12 @@ import createTestValidation from './createTest'
 import editTestValidation from './editTest'
 import loginUserValidation from './loginUser'
 import createCustomerValidation from './createCustomer'
+import createEmployeeValidation from './createEmployee'
 
 export {
   createTestValidation,
   editTestValidation,
   loginUserValidation,
   createCustomerValidation,
+  createEmployeeValidation,
 }
