9b35027ff5ec61407cde3f83a7ba32d0395d6213
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-07 23:37:29 +0800

    Add EmployeeDashboard

---
+++ src/screens/EmployeeDashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Tex } from "react-native";
+
+export default function () {
+
+  return (
+    <>
+      <View className={`flex-1 justify-center items-center`}>
+          <Text>Employee Dashboard</Text>
+      </View>
+    </>
+  );
+}
