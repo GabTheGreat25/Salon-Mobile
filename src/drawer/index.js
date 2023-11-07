3a9b555b3283e1667e219ee2fa9c6685cb91f49f
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-07 23:35:09 +0800

    Import all drawer

---
+++ src/drawer/index.js
@@ -0,0 +1,5 @@
+import AdminDrawer from "./AdminDrawer";
+import EmployeeDrawer from "./EmployeeDrawer";
+import CustomerDrawer from "./CustomerDrawer";
+
+export { AdminDrawer, EmployeeDrawer, CustomerDrawer };
