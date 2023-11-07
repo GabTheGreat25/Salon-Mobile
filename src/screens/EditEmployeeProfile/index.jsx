f219ed699e65103890f9ae516633c332ee2afd67
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-07 23:37:20 +0800

    Add EditEmployeeProfile

---
+++ src/screens/EditEmployeeProfile/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function () {
+
+  return (
+    <>
+      <View className={`flex-1 justify-center items-center`}>
+          <Text>Edit Employee Profile</Text>
+      </View>
+    </>
+  );
+}
