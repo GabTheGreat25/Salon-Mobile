2a9bd812a126fc863635d11694c5cb54bb79ff65
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-07 23:36:22 +0800

    Add AdminDashboard

---
+++ src/screens/AdminDashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function () {
+
+  return (
+    <>
+      <View className={`flex-1 justify-center items-center`}>
+          <Text>Admin Dashboard</Text>
+      </View>
+    </>
+  );
+}
