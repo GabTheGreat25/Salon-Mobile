798352b1d1cb05bfb862592e739d8234450934be
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-10 16:25:17 +0800

    Add extra logic to get other tables in state

--- src/state/auth/authReducer.js
+++ src/state/auth/authReducer.js
@@ -20,10 +20,54 @@ export const authSlice = createSlice({
       (state, { payload }) => {
         if (payload?.success === true) {
           state.token = payload?.details?.accessToken;
-          state.user = payload?.details?.user;
           state.authenticated = true;
-          state.loggedInUserId = state?.user?._id;
+          state.loggedInUserId = payload?.details?.user?._id;
+
+          const roles = payload?.details?.user?.roles;
+          if (roles) {
+            if (roles.includes("Employee")) {
+              state.user = {
+                ...state.user,
+                ...payload?.details?.user,
+                requirement: payload?.details?.requirement,
+              };
+            } else if (roles.includes("Online Customer") || roles.includes("Walk-In Customer")) {
+              state.user = {
+                ...state.user,
+                ...payload?.details?.user,
+                information: payload?.details?.information,
+              };
+            } else {
+              state.user = {
+                ...state.user,
+                ...payload?.details?.user,
+              };
+            }
+          } else {
+            state.user = {
+              ...state.user,
+              ...payload?.details?.user,
+            };
+          }
         }
+      }
+    );
+    builder.addMatcher(
+      api.endpoints.updateUser.matchFulfilled,
+      (state, { payload }) => {
+        if (payload?.success === true) {
+          const updatedUser = payload?.details?.updatedUser;
+          if (updatedUser?._id === state.loggedInUserId) {
+            return {
+              ...state,
+              user: {
+                ...state.user,
+                ...updatedUser,
+              },
+            };
+          }
+        }
+        return state;
       }
     );
   },
