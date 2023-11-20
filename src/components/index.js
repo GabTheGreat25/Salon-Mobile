be5fb50a3c8b3b0b8ae29c2d6488900260b85977
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-20 23:41:58 +0800

    Import ListData

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,14 @@ import Signup from "./Signup";
 import Login from "./Login";
 import LoadingScreen from "./LoadingScreen";
 import UserImage from "./UserImage";
+import ListData from "./ListData";
 
-export { Welcome, Opening, Signup, Login, LoadingScreen, UserImage };
+export {
+    Welcome,
+    Opening,
+    Signup,
+    Login,
+    LoadingScreen,
+    UserImage,
+    ListData
+};
