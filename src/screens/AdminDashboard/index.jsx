9fc871c665add65f701a61cc968e64beb6d26c9d
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-21 19:18:20 +0800

    Remove show all users and add ShowPendingEmployee chart

--- src/screens/AdminDashboard/index.jsx
+++ src/screens/AdminDashboard/index.jsx
@@ -4,20 +4,26 @@ import { View, ScrollView } from "react-native";
 import { useGetUsersQuery } from "../../state/api/reducer";
 import { Feather } from "@expo/vector-icons";
 import { changeColor, dimensionLayout } from "@utils";
+import ShowPendingEmployees from "./ShowPendingEmployees";
+import randomColor from "randomcolor";
 
 export default function () {
   const { data, isLoading } = useGetUsersQuery();
   const users = data?.details ?? [];
-  const usersFilter = users?.filter((user) => user?.active === true);
-  const usersCount = usersFilter.length;
+
   const admins = users?.filter((user) => user?.roles?.includes("Admin"));
   const adminCount = admins.length;
-  const employees = users?.filter((user) => user?.roles?.includes("Employee") && user?.active === true);
+
+  const employees = users?.filter(
+    (user) => user?.roles?.includes("Employee") && user?.active === true
+  );
   const employeeCount = employees.length;
+
   const customers = users?.filter((user) =>
     user?.roles?.includes("Online Customer")
   );
   const customerCount = customers.length;
+
   const { textColor, backgroundColor } = changeColor();
   const isDimensionLayout = dimensionLayout();
 
@@ -35,42 +41,35 @@ export default function () {
             <ScrollView horizontal>
               <View className={`flex flex-row justify-center items-center`}>
                 <ListData
-                  title="Users"
-                  data={usersCount}
-                  icon={<Feather name="users" size={40} color={textColor} />}
-                  id={data?.details?._id}
-                  backgroundColor={"#FDA7DF"}
-                />
-                <View style={{ width: 10 }} />
-                <ListData
                   title="Admin"
                   data={adminCount}
                   icon={<Feather name="user" size={40} color={textColor} />}
                   id={data?.details?._id}
-                  backgroundColor={"#F78FB3"}
+                  backgroundColor={randomColor({ luminosity: "bright" })}
                 />
                 <View style={{ width: 10 }} />
                 <ListData
                   title="Employee"
                   data={employeeCount}
-                  icon={
-                    <Feather name="briefcase" size={40} color={textColor} />
-                  }
+                  icon={<Feather name="users" size={40} color={textColor} />}
                   id={data?.details?._id}
-                  backgroundColor={"#FDA7DF"}
+                  backgroundColor={randomColor({ luminosity: "bright" })}
                 />
                 <View style={{ width: 10 }} />
                 <ListData
                   title="Customer"
                   data={customerCount}
                   icon={
-                    <Feather name="shopping-bag" size={40} color={textColor} />
+                    <Feather name="user-check" size={40} color={textColor} />
                   }
                   id={data?.details?._id}
-                  backgroundColor={"#F78FB3"}
+                  backgroundColor={randomColor({ luminosity: "bright" })}
                 />
               </View>
             </ScrollView>
+            <View>
+              <ShowPendingEmployees />
+            </View>
           </ScrollView>
         </>
       )}
