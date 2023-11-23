8aecdb8951cbcdb0ed2b254b5281e4f54c3a0c88
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-23 23:56:11 +0800

    Add Product

---
+++ src/screens/Product/index.jsx
@@ -0,0 +1,300 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  Alert,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
+import {
+  useGetProductsQuery,
+  useDeleteProductMutation,
+} from "../../state/api/reducer";
+import { LoadingScreen } from "@components";
+import Toast from "react-native-toast-message";
+import { BackIcon } from "@helpers";
+import { DataTable } from "react-native-paper";
+import { Feather } from "@expo/vector-icons";
+import { changeColor, dimensionLayout } from "@utils";
+import { useNavigation } from "@react-navigation/native";
+import {
+  saveDeletedProductId,
+  getDeletedProductIds,
+} from "../../helpers/DeleteItem";
+
+export default function () {
+  const isDimensionLayout = dimensionLayout();
+  const navigation = useNavigation();
+  const { width: deviceWidth } = Dimensions.get("window");
+  const customWidth = deviceWidth * (isDimensionLayout ? 0.3 : 0.2);
+
+  const { data, isLoading, refetch } = useGetProductsQuery();
+  const { backgroundColor, textColor, colorScheme } = changeColor();
+
+  const borderColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
+  const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
+
+  const [deleteProduct, { isLoading: isDeleting }] = useDeleteProductMutation();
+
+  const [deletedIds, setDeletedIds] = useState([]);
+
+  useEffect(() => {
+    const fetchDeletedIds = async () => {
+      const ids = await getDeletedProductIds();
+      setDeletedIds(ids);
+    };
+
+    fetchDeletedIds();
+  }, []);
+
+  const handleDeleteProduct = async (id) => {
+    Alert.alert(
+      "Delete Product",
+      "Are you sure you want to delete this product?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: async () => {
+            try {
+              const response = await deleteProduct(id).unwrap();
+              await saveDeletedProductId(id);
+              setDeletedIds((prevIds) => [...prevIds, id]);
+              refetch();
+              Toast.show({
+                type: "success",
+                position: "top",
+                text1: "Product Successfully Deleted",
+                text2: `${response?.message}`,
+                visibilityTime: 3000,
+                autoHide: true,
+              });
+            } catch (error) {
+              Toast.show({
+                type: "error",
+                position: "top",
+                text1: "Error Deleting Product",
+                text2: `${error?.data?.error?.message}`,
+                visibilityTime: 3000,
+                autoHide: true,
+              });
+            }
+          },
+        },
+      ]
+    );
+  };
+
+  const createProduct = () => navigation.navigate("CreateProduct");
+
+  const filteredData =
+    data?.details?.filter((item) => !deletedIds.includes(item?._id)) || [];
+
+  return (
+    <>
+      {isLoading || isDeleting ? (
+        <View
+          className={`flex-1 justify-center items-center bg-primary-default`}
+        >
+          <LoadingScreen />
+        </View>
+      ) : (
+        <SafeAreaView style={{ backgroundColor }} className={`relative flex-1`}>
+          <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
+          <View className={`ml-12 mt-[14px] items-start justify-start`}>
+            <TouchableOpacity
+              style={{ backgroundColor: borderColor }}
+              className={`py-1 px-3 rounded-md border `}
+              onPress={createProduct}
+            >
+              <Text
+                style={{ color: invertTextColor }}
+                className={`text-lg font-semibold`}
+              >
+                Create Product
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            className={`flex-1 items-center justify-center ${
+              isDimensionLayout ? "mt-10" : "my-7"
+            }`}
+          >
+            {filteredData?.length ? (
+              <ScrollView
+                style={{ backgroundColor }}
+                showsVerticalScrollIndicator={false}
+              >
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  <DataTable>
+                    <DataTable.Header
+                      style={{
+                        backgroundColor,
+                        borderBottomWidth: 1,
+                        borderBottomColor: borderColor,
+                      }}
+                    >
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Product Name</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Brand</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Type</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Images</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Actions</Text>
+                      </DataTable.Title>
+                    </DataTable.Header>
+                    {filteredData?.map((item) => (
+                      <DataTable.Row
+                        key={item?._id}
+                        style={{
+                          backgroundColor,
+                          borderBottomWidth: 1,
+                          borderBottomColor: borderColor,
+                        }}
+                      >
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.product_name}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.brand}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.type}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            paddingBottom: 24,
+                          }}
+                        >
+                          {item?.image?.map((image) => (
+                            <Image
+                              key={image?.public_id}
+                              source={{ uri: image?.url }}
+                              style={{ width: 100, height: 75 }}
+                              resizeMode="cover"
+                            />
+                          ))}
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            width: customWidth,
+                            justifyContent: "space-around",
+                            alignItems: "center",
+                            padding: 10,
+                          }}
+                        >
+                          <TouchableOpacity
+                            onPress={() => handleDeleteProduct(item?._id)}
+                          >
+                            <Feather name="delete" size={24} color="red" />
+                          </TouchableOpacity>
+                        </DataTable.Cell>
+                      </DataTable.Row>
+                    ))}
+                  </DataTable>
+                </ScrollView>
+              </ScrollView>
+            ) : (
+              <View
+                className={`flex-1 justify-center items-center`}
+                style={{ backgroundColor }}
+              >
+                <Text style={{ color: textColor }}>No data available.</Text>
+              </View>
+            )}
+          </View>
+        </SafeAreaView>
+      )}
+    </>
+  );
+}
