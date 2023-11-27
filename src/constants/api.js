219a3e42c325c16286bfb8109b2d817e9c63be53
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-11-28 00:48:35 +0800

    Add delivery api constant

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,16 +1,17 @@
 import TAGS from "./tags";
 
 export default {
-    GET: "GET",
-    POST: "POST",
-    PATCH: "PATCH",
-    PUT: "PUT",
-    DELETE: "DELETE",
-    TAGS: [
-        TAGS.API,
-        TAGS.TESTS,
-        TAGS.USERS,
-        TAGS.AUTH,
-        TAGS.PRODUCTS,
-    ],
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+  TAGS: [
+    TAGS.API,
+    TAGS.TESTS,
+    TAGS.USERS,
+    TAGS.AUTH,
+    TAGS.PRODUCTS,
+    TAGS.DELIVERY,
+  ],
 };
