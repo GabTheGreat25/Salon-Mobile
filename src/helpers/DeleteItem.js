ce9fcb52c10b1e389bf56d8313c5b150864cd9b3
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-11-28 00:49:14 +0800

    Add global delete item

--- src/helpers/DeleteItem.js
+++ src/helpers/DeleteItem.js
@@ -1,12 +1,12 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const saveDeletedProductId = async (id) => {
-    const deletedIds = await AsyncStorage.getItem('deletedIds');
-    const updatedDeletedIds = [...JSON.parse(deletedIds || '[]'), id];
-    await AsyncStorage.setItem('deletedIds', JSON.stringify(updatedDeletedIds));
+export const saveDeletedId = async (id) => {
+  const deletedIds = await AsyncStorage.getItem("deletedIds");
+  const updatedDeletedIds = [...JSON.parse(deletedIds || "[]"), id];
+  await AsyncStorage.setItem("deletedIds", JSON.stringify(updatedDeletedIds));
 };
 
-export const getDeletedProductIds = async () => {
-    const deletedIds = await AsyncStorage.getItem('deletedIds');
-    return JSON.parse(deletedIds || '[]');
+export const getDeletedIds = async () => {
+  const deletedIds = await AsyncStorage.getItem("deletedIds");
+  return JSON.parse(deletedIds || "[]");
 };
