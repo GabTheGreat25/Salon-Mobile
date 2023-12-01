12ee1504c0edd62acc7e8532776ca0bf7e18705a
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-01 17:40:35 +0800

    Add shopping icon for customer

--- src/helpers/DarkMode/index.jsx
+++ src/helpers/DarkMode/index.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import { RESOURCE } from "@constants";
+import { changeColor } from "@utils";
+import { useSelector } from "react-redux";
 
 export default function ({ name, color, toggle }) {
+  const userRoles = useSelector((state) => state.auth?.user?.roles);
+  const { textColor } = changeColor();
+
+  const isOnlineCustomer = userRoles?.includes("Online Customer");
+
   return (
     <>
-      <TouchableOpacity
-        className="absolute z-[1000] right-5 top-4"
-        onPress={toggle}
-      >
-        <Text selectable={false}>
-          <Feather name={name} size={25} color={color} />
-        </Text>
-      </TouchableOpacity>
+      {isOnlineCustomer && (
+        <>
+          <TouchableOpacity
+            className="absolute z-[1000] right-14 top-4"
+            onPress={toggle}
+          >
+            <Text selectable={false}>
+              <Feather name="shopping-bag" size={25} color={textColor} />
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="absolute z-[1000] right-5 top-4"
+            onPress={toggle}
+          >
+            <Text selectable={false}>
+              <Feather name={name} size={25} color={color} />
+            </Text>
+          </TouchableOpacity>
+        </>
+      )}
+      {!isOnlineCustomer && (
+        <TouchableOpacity
+          className="absolute z-[1000] right-5 top-4"
+          onPress={toggle}
+        >
+          <Text selectable={false}>
+            <Feather name={name} size={25} color={color} />
+          </Text>
+        </TouchableOpacity>
+      )}
     </>
   );
 }
