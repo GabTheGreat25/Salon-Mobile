459493ea1d38142a4259fa6a1d2895cd99c22b16
Jay200123 <kenken.ayonon@gmail.com>
2023-12-03 23:59:58 +0800

    import & export settings UI

---
+++ src/settings/index.js
@@ -0,0 +1,9 @@
+import TermsConditions from "./TermsConditions";
+import PrivacyPolicy from "./PrivacyPolicy";
+import Feedback from "./Feedback";
+
+export {
+    TermsConditions,
+    PrivacyPolicy,
+    Feedback
+}
