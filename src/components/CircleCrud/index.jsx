74b707d7f49e2820ad3e22e1c099fd193a63fdb9
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:27:32 +0800

    Refactor CircleCrud

--- src/components/CircleCrud/index.jsx
+++ src/components/CircleCrud/index.jsx
@@ -3,37 +3,36 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { changeColor } from "@utils";
+import { te } from "date-fns/locale";
 
 export default function (props) {
   const { icon, title, routeName, backgroundColor } = props;
   const navigation = useNavigation();
-  const { textColor } = changeColor();
+  const { textColor, colorScheme } = changeColor();
+  const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
 
   const handlePress = () => {
     navigation.navigate(routeName);
   };
 
   return (
-      <TouchableOpacity
-        className={`flex-1 justify-center items-center text-center my-5`}
-        onPress={handlePress}
+    <TouchableOpacity
+      className={`flex-1 justify-center items-center text-center my-5`}
+      onPress={handlePress}
+    >
+      <View
+        style={{
+          width: 60,
+          height: 60,
+          backgroundColor: backgroundColor,
+        }}
+        className={`flex justify-center items-center rounded-full mb-[5px]`}
       >
-        <View
-          style={{
-            width: 60,
-            height: 60,
-            backgroundColor: backgroundColor,
-          }}
-          className={`flex justify-center items-center rounded-full mb-[5px]`}
-        >
-          <Feather name={icon} size={24} color={textColor} />
-        </View>
-        <Text
-          className={`text-base font-semibold`}
-          style={{ color: textColor }}
-        >
-          {title}
-        </Text>
-      </TouchableOpacity>
+        <Feather name={icon} size={24} color={invertTextColor} />
+      </View>
+      <Text className={`text-base font-semibold`} style={{ color: textColor }}>
+        {title}
+      </Text>
+    </TouchableOpacity>
   );
 }
