339baf314fa58267fbf74b1608f1bf89904ac0cd
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:31:41 +0800

    Refactor BecomeEmployee

--- src/screens/BecomeEmployee/index.jsx
+++ src/screens/BecomeEmployee/index.jsx
@@ -9,7 +9,7 @@ export default function () {
   return (
     <>
       <Welcome
-        title="Become a Lanlee Employee?"
+        title="Become a Lanlee Beautician?"
         description={`Lorem ipsum dolor sit amet \nconsectetur adipisicing elit.`}
         buttonTitle="Learn More"
         leftArrow={true}
