c320888ce7ee8004159716d5a85ae08ec1afff1b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:44:29 +0800

    refactor createService

--- src/validation/createService.js
+++ src/validation/createService.js
@@ -1,18 +1,23 @@
 import * as yup from "yup";
 
 export default yup.object({
+  product: yup
+    .array()
+    .of(yup.string().required("Product required"))
+    .required("At least one product is required"),
   service_name: yup
-    .string("Enter your Service Name")
+    .string()
     .required("Service Name is required")
-    .min(2, "Your service Name should be of minimum 2 characters legth")
-    .max(30, "Your service Name cannot exceed 30 characters"),
-  description: yup
-    .string("Enter your description")
-    .min(2, "Your service Name should be of minimum 2 characters legth")
-    .required("Description is required"),
+    .max(60, "Service Name must not exceed 60 characters"),
+  description: yup.string().required("Description is required"),
   price: yup
-    .number("Enter a price")
+    .number()
     .required("Price is required")
-    .min(0, "Price must be at least 0")
-    .max(10000, "Price must be at most 10000"),
+    .min(0, "Price must be a positive number"),
+  type: yup
+    .array()
+    .of(yup.string().required("Service Type required"))
+    .required("At least one service type is required"),
+  occassion: yup.string(),
+  duration: yup.string().required("Duration is required"),
 });
