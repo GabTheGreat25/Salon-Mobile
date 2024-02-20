f270a3e72d0d951d03f52d5622b66a7280cd4778
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:48:52 +0800

    Refactor updatePassword validation

--- src/validation/updatePassword.js
+++ src/validation/updatePassword.js
@@ -3,11 +3,11 @@ import * as yup from "yup";
 export default yup.object({
   oldPassword: yup
     .string("Enter your old password")
-    .min(8, "Password must be at least 8 characters long")
+    .min(6, "Password must be at least 6 characters long")
     .required("Old Password is required"),
   newPassword: yup
     .string("Enter your new password")
-    .min(8, "Password must be at least 8 characters long")
+    .min(6, "Password must be at least 6 characters long")
     .required("New Password is required"),
   confirmPassword: yup
     .string("Enter your confirm password")
