37ee4274d19a1568c339de304e4adb27bca2e9da
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:34:32 +0800

    Refactor LoginUser

--- src/screens/LoginUser/index.jsx
+++ src/screens/LoginUser/index.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { dimensionLayout } from "@utils";
 import { Login } from "@components";
-import Facebook from "@assets/facebook.png";
-import Google from "@assets/google.png";
-import Instagram from "@assets/instagram.png";
-import Linkedin from "@assets/linkedin.png";
 import { useNavigation } from "@react-navigation/native";
 
 export default function () {
   const navigation = useNavigation();
-  const isDimensionLayout = dimensionLayout();
-
   return (
     <>
       <Login
@@ -24,7 +17,6 @@ export default function () {
         footerTitle={`Don't have an account?`}
         footerLink={() => navigation.navigate("ChooseRole")}
         footerLinkTitle={`Sign up here`}
-        dimensionLayout={isDimensionLayout}
       />
     </>
   );
