840de81f3a6ff6f2e07e67be181ce04a5231228f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:47:29 +0800

    refactor editService validation

--- src/validation/editService.js
+++ src/validation/editService.js
@@ -2,17 +2,13 @@ import * as yup from "yup";
 
 export default yup.object({
   service_name: yup
-    .string("Enter your Service Name")
-    .required("Service Name is required")
-    .min(2, "Your service Name should be of minimum 2 characters legth")
-    .max(30, "Your service Name cannot exceed 30 characters"),
+    .string("Enter Service Name")
+    .required("Service Name is required"),
   description: yup
-    .string("Enter your description")
-    .min(2, "Your service Name should be of minimum 2 characters legth")
+    .string("Enter Service Description")
     .required("Description is required"),
-  price: yup
-    .number("Enter a price")
-    .required("Price is required")
-    .min(0, "Price must be at least 0")
-    .max(10000, "Price must be at most 10000"),
+  price: yup.string("Enter Service Price").required("Price required"),
+  type: yup.string(),
+  occassion: yup.string().required("Occassion is required"),
+  duration: yup.string().required("Duration is required"),
 });
