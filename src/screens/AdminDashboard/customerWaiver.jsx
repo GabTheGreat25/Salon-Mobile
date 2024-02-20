80ece29fa680a9a117b4b50518dc94879dff207f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:30:47 +0800

    Add customerWaiver

---
+++ src/screens/AdminDashboard/customerWaiver.jsx
@@ -0,0 +1,326 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import { useGetUsersQuery } from "../../state/api/reducer";
+import { LoadingScreen } from "@components";
+import { DataTable } from "react-native-paper";
+import { changeColor } from "@utils";
+import { useIsFocused } from "@react-navigation/native";
+
+export default function () {
+  const isFocused = useIsFocused();
+  const { width: deviceWidth } = Dimensions.get("window");
+  const customWidth = deviceWidth * 0.3;
+
+  const { data, isLoading, refetch } = useGetUsersQuery();
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) refetch();
+    };
+    fetchData();
+  }, [isFocused]);
+
+  const users = data?.details;
+  const { backgroundColor, textColor, colorScheme } = changeColor();
+
+  const borderColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
+
+  const [page, setPage] = useState(0);
+  const itemsPerPage = 7;
+
+  const filteredUser = users?.filter(
+    (user) =>
+      (user.roles.includes("Online Customer") ||
+        user.roles.includes("Walk-in Customer")) &&
+      !user.information.allergy.includes("None") &&
+      !user.information.allergy.includes("Others")
+  );
+
+  const totalPageCount = Math.ceil(filteredUser.length / itemsPerPage);
+  const paginatedData = filteredUser.slice(
+    page * itemsPerPage,
+    (page + 1) * itemsPerPage
+  );
+
+  const handleNextPage = () => {
+    if (page < totalPageCount - 1) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <View
+          className={`flex-1 justify-center items-center bg-primary-default`}
+        >
+          <LoadingScreen />
+        </View>
+      ) : (
+        <SafeAreaView style={{ backgroundColor }} className={`relative flex-1`}>
+          <View className={`flex-1 items-center justify-center pt-4`}>
+            {paginatedData?.length ? (
+              <ScrollView
+                style={{ backgroundColor }}
+                showsVerticalScrollIndicator={false}
+              >
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  <DataTable>
+                    <DataTable.Header
+                      style={{
+                        backgroundColor,
+                        borderBottomWidth: 1,
+                        borderBottomColor: borderColor,
+                      }}
+                    >
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>ID</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Customer Name</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Email</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Contact Number</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>Role</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>
+                          Ingredients Exclusion
+                        </Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>E Signature</Text>
+                      </DataTable.Title>
+                    </DataTable.Header>
+                    {paginatedData?.map((item) => (
+                      <DataTable.Row
+                        key={item?._id}
+                        style={{
+                          backgroundColor,
+                          borderBottomWidth: 1,
+                          borderBottomColor: borderColor,
+                        }}
+                      >
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?._id}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.name}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.email}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.contact_number}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.roles}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.information?.allergy.join(", ")}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Image
+                            source={{ uri: item?.information?.eSignature }}
+                            style={{
+                              width: 140,
+                              height: 50,
+                              padding: 2,
+                              margin: 2,
+                              borderRadius: 10,
+                            }}
+                          />
+                        </DataTable.Cell>
+                      </DataTable.Row>
+                    ))}
+                  </DataTable>
+                </ScrollView>
+              </ScrollView>
+            ) : (
+              <View
+                className={`flex-1 justify-center items-center`}
+                style={{ backgroundColor }}
+              >
+                <Text style={{ color: textColor }}>No data available.</Text>
+              </View>
+            )}
+            {paginatedData?.length ? (
+              <View className={`flex items-center flex-row my-6`}>
+                <Button
+                  title="Previous"
+                  onPress={handlePrevPage}
+                  disabled={page === 0}
+                  color="#FDA7DF"
+                />
+                <Text
+                  style={{
+                    color: textColor,
+                  }}
+                  className={`px-20`}
+                >{`Page ${page + 1} of ${totalPageCount}`}</Text>
+                <Button
+                  title="Next"
+                  onPress={handleNextPage}
+                  disabled={page === totalPageCount - 1}
+                  color="#FDA7DF"
+                />
+              </View>
+            ) : null}
+          </View>
+        </SafeAreaView>
+      )}
+    </>
+  );
+}
