b5ff9c695733e300fe873ec1fd48f275491c8473
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:28:05 +0800

    Refactor Opening

--- src/components/Opening/index.jsx
+++ src/components/Opening/index.jsx
@@ -21,7 +21,6 @@ export default function ({
   secondButton,
   navigateSecondButton,
   navigateBack,
-  dimensionLayout,
 }) {
   const { backgroundColor, textColor, colorScheme } = changeColor();
   const imageSource = colorScheme === "dark" ? salonLogoWhite : salonLogo;
@@ -29,40 +28,22 @@ export default function ({
     <>
       <SafeAreaView
         style={{ backgroundColor }}
-        className={`relative flex-1 ${
-          dimensionLayout ? "flex-col" : "flex-row"
-        }`}
+        className={`relative flex-1 flex-col`}
       >
         <BackIcon navigateBack={navigateBack} textColor={textColor} />
-        <View
-          className={`flex-1 justify-center items-center ${
-            dimensionLayout ? "flex-col" : "flex-row"
-          }`}
-        >
-          <View
-            className={`flex-1 ${
-              dimensionLayout
-                ? "justify-end items-center"
-                : "justify-start items-end"
-            }`}
-          >
+        <View className={`flex-1 justify-start items-center flex-col`}>
+          <View className={`flex-1 justify-end items-start`}>
             <Image
               source={imageSource}
-              className={`${dimensionLayout ? "mb-0" : "mb-5"}`}
+              className={`mb-0`}
               resizeMode="contain"
             />
           </View>
-          <View
-            className={`items-center ${
-              dimensionLayout ? "justify-start" : "justify-center"
-            } flex-1`}
-          >
+          <View className={`items-center justify-start flex-1`}>
             {showTitle && (
               <Text
                 style={{ color: textColor }}
-                className={`font-base mb-2 text-2xl ${
-                  dimensionLayout ? "mt-6" : "mt-0"
-                }`}
+                className={`font-base mb-2 text-3xl mt-8`}
               >
                 {title}
               </Text>
@@ -76,26 +57,28 @@ export default function ({
               </Text>
             )}
             <View>
-              <TouchableOpacity
-                className={`mt-5`}
-                onPress={navigateFirstButton}
-              >
-                <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
-                  <Text
-                    className={`text-neutral-light font-semibold text-center text-lg`}
-                    style={{ color: textColor }}
-                  >
-                    {firstButton}
-                  </Text>
-                </View>
-              </TouchableOpacity>
+              {firstButton !== null && (
+                <TouchableOpacity
+                  className={`mt-5`}
+                  onPress={navigateFirstButton}
+                >
+                  <View className={`px-14 py-2 rounded-3xl bg-primary-accent`}>
+                    <Text
+                      className={`text-neutral-light font-normal text-center text-2xl`}
+                      style={{ color: textColor }}
+                    >
+                      {firstButton}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              )}
               <TouchableOpacity
                 className={`mt-8`}
                 onPress={navigateSecondButton}
               >
-                <View className={`px-12 py-2 rounded-3xl bg-primary-accent`}>
+                <View className={`px-14 py-2 rounded-3xl bg-primary-accent`}>
                   <Text
-                    className={`text-neutral-light font-semibold text-center text-lg`}
+                    className={`text-neutral-light font-normal text-center text-2xl`}
                     style={{ color: textColor }}
                   >
                     {secondButton}
