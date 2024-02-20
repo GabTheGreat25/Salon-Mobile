036d09d442e6476b484936374b73c5bcbf864fa1
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:37:58 +0800

    Add hiring

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth/authReducer";
 import appointment from "./appointment/appointmentReducer";
+import hiring from "./hiring/hiringReducer";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth,
   appointment,
+  hiring,
   [api.reducerPath]: api.reducer,
 });
