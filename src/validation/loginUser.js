222053fc9956751fec5da59f2c45ca9295baf319
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:48:31 +0800

    Refactor loginUser validation

--- src/validation/loginUser.js
+++ src/validation/loginUser.js
@@ -1,9 +1,12 @@
 import * as yup from "yup";
 
 export default yup.object({
-    email: yup.string("Enter your email").email("Enter a valid email").required("Email is required"),
-    password: yup
-        .string("Enter your password")
-        .required("Password is required")
-        .min(8, "Password should be of minimum 8 characters length"),
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(6, "Password should be of minimum 6 characters length")
+    .required("Password is required"),
 });
