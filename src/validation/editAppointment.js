d1e613c5d3f9d8a628c0e2f56c1297405d6e8165
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:45:32 +0800

    refactor editAppoinment validation

--- src/validation/editAppointment.js
+++ src/validation/editAppointment.js
@@ -1,11 +1,9 @@
 import * as yup from "yup";
 
 export default yup.object({
-  date: yup.string().required("Date is required"),
-  time: yup.string().required("Time is required"),
   price: yup
-    .number("Enter a price")
+    .number()
     .required("Price is required")
-    .min(0, "Price must be at least 0")
-    .max(10000, "Price must be at most 10000"),
+    .min(0, "Price must be a positive number"),
+  service: yup.array().of(yup.string()).required("Service is required"),
 });
