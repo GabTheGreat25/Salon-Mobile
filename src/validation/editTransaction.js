5c973b46e8df489107a522a32e4e687ede03d12c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:47:48 +0800

    refactor editTransaction validation

--- src/validation/editTransaction.js
+++ src/validation/editTransaction.js
@@ -1,7 +1,5 @@
 import * as yup from "yup";
 
 export default yup.object({
-  date: yup.string().required("Date is required"),
-  status: yup.string().required("Time is required"),
-  payment: yup.string().required("Payment is required"),
+  status: yup.string().required("Transaction Status required"),
 });
