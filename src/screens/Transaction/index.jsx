7dc080e50225ba5b5da4479ce57a7bd7a3dee559
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-20 19:37:23 +0800

    Refactor Transaction

--- src/screens/Transaction/index.jsx
+++ src/screens/Transaction/index.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
-  Image,
   Alert,
   TouchableOpacity,
   ScrollView,
   Dimensions,
   SafeAreaView,
+  Image,
+  Button,
 } from "react-native";
 import {
   useGetTransactionsQuery,
@@ -18,30 +19,35 @@ import Toast from "react-native-toast-message";
 import { BackIcon } from "@helpers";
 import { DataTable } from "react-native-paper";
 import { Feather } from "@expo/vector-icons";
-import { changeColor, dimensionLayout } from "@utils";
+import { changeColor } from "@utils";
 import { useNavigation } from "@react-navigation/native";
 import { saveDeletedId, getDeletedIds } from "../../helpers/DeleteItem";
-import { format } from "date-fns";
-import { useSelector } from "react-redux";
+import { useIsFocused } from "@react-navigation/native";
 
 export default function () {
-  const isDimensionLayout = dimensionLayout();
+  const isFocused = useIsFocused();
   const navigation = useNavigation();
   const { width: deviceWidth } = Dimensions.get("window");
-  const customWidth = deviceWidth * (isDimensionLayout ? 0.3 : 0.2);
+  const customWidth = deviceWidth * 0.3;
 
   const { data, isLoading, refetch } = useGetTransactionsQuery();
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) refetch();
+    };
+    fetchData();
+  }, [isFocused]);
+
   const { backgroundColor, textColor, colorScheme } = changeColor();
 
   const borderColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
-  const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
 
   const [deleteTransaction, { isLoading: isDeleting }] =
     useDeleteTransactionMutation();
 
   const [deletedIds, setDeletedIds] = useState([]);
-
-  const roles = useSelector((state) => state.auth.user.roles);
+  const [page, setPage] = useState(0);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchDeletedIds = async () => {
@@ -100,6 +106,24 @@ export default function () {
   const filteredData =
     data?.details?.filter((item) => !deletedIds.includes(item?._id)) || [];
 
+  const totalPageCount = Math.ceil(filteredData.length / itemsPerPage);
+  const paginatedData = filteredData.slice(
+    page * itemsPerPage,
+    (page + 1) * itemsPerPage
+  );
+
+  const handleNextPage = () => {
+    if (page < totalPageCount - 1) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
   return (
     <>
       {isLoading || isDeleting ? (
@@ -111,12 +135,8 @@ export default function () {
       ) : (
         <SafeAreaView style={{ backgroundColor }} className={`relative flex-1`}>
           <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
-          <View
-            className={`flex-1 items-center justify-center ${
-              isDimensionLayout ? "mt-10" : "my-7"
-            }`}
-          >
-            {filteredData?.length ? (
+          <View className={`flex-1 items-center justify-center pt-12`}>
+            {paginatedData?.length ? (
               <ScrollView
                 style={{ backgroundColor }}
                 showsVerticalScrollIndicator={false}
@@ -138,7 +158,7 @@ export default function () {
                           width: customWidth,
                         }}
                       >
-                        <Text style={{ color: textColor }}>Appointment</Text>
+                        <Text style={{ color: textColor }}>ID</Text>
                       </DataTable.Title>
                       <DataTable.Title
                         style={{
@@ -148,7 +168,7 @@ export default function () {
                           width: customWidth,
                         }}
                       >
-                        <Text style={{ color: textColor }}>Date</Text>
+                        <Text style={{ color: textColor }}>Payment Method</Text>
                       </DataTable.Title>
                       <DataTable.Title
                         style={{
@@ -168,7 +188,31 @@ export default function () {
                           width: customWidth,
                         }}
                       >
-                        <Text style={{ color: textColor }}>Payment</Text>
+                        <Text style={{ color: textColor }}>Customer Name</Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>
+                          Appointment Day
+                        </Text>
+                      </DataTable.Title>
+                      <DataTable.Title
+                        style={{
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 10,
+                          width: customWidth,
+                        }}
+                      >
+                        <Text style={{ color: textColor }}>
+                          PWD / Senior Citizen ID Image
+                        </Text>
                       </DataTable.Title>
                       <DataTable.Title
                         style={{
@@ -181,7 +225,7 @@ export default function () {
                         <Text style={{ color: textColor }}>Actions</Text>
                       </DataTable.Title>
                     </DataTable.Header>
-                    {filteredData?.map((item) => (
+                    {paginatedData?.map((item) => (
                       <DataTable.Row
                         key={item?._id}
                         style={{
@@ -203,55 +247,7 @@ export default function () {
                             numberOfLines={1}
                             ellipsizeMode="tail"
                           >
-                            {item?.appointment?.price}
-                          </Text>
-                        </DataTable.Cell>
-                        <DataTable.Cell
-                          style={{
-                            justifyContent: "center",
-                            alignItems: "center",
-                            padding: 10,
-                            width: customWidth,
-                          }}
-                        >
-                          <Text
-                            style={{ color: textColor }}
-                            numberOfLines={1}
-                            ellipsizeMode="tail"
-                          >
-                            {item?.employee?.name}
-                          </Text>
-                        </DataTable.Cell>
-                        <DataTable.Cell
-                          style={{
-                            justifyContent: "center",
-                            alignItems: "center",
-                            padding: 10,
-                            width: customWidth,
-                          }}
-                        >
-                          <Text
-                            style={{ color: textColor }}
-                            numberOfLines={1}
-                            ellipsizeMode="tail"
-                          >
-                            {format(new Date(item?.date), "yyyy-MM-dd")}
-                          </Text>
-                        </DataTable.Cell>
-                        <DataTable.Cell
-                          style={{
-                            justifyContent: "center",
-                            alignItems: "center",
-                            padding: 10,
-                            width: customWidth,
-                          }}
-                        >
-                          <Text
-                            style={{ color: textColor }}
-                            numberOfLines={1}
-                            ellipsizeMode="tail"
-                          >
-                            {item.status}
+                            {item?._id}
                           </Text>
                         </DataTable.Cell>
                         <DataTable.Cell
@@ -272,6 +268,104 @@ export default function () {
                         </DataTable.Cell>
                         <DataTable.Cell
                           style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.status}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item?.appointment?.customer?.name}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item.appointment?.date
+                              ? `${
+                                  new Date(item.appointment?.date)
+                                    .toISOString()
+                                    .split("T")[0]
+                                } ${
+                                  item.appointment?.time
+                                    ? item.appointment?.time[0] +
+                                      " - " +
+                                      item.appointment?.time[
+                                        item.appointment?.time?.length - 1
+                                      ]
+                                    : ""
+                                }`
+                              : ""}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text
+                            style={{ color: textColor }}
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                          >
+                            {item.image?.length > 0 ? (
+                              <Image
+                                key={
+                                  item.image[
+                                    Math.floor(
+                                      Math.random() * item.image?.length
+                                    )
+                                  ]?.public_id
+                                }
+                                source={{
+                                  uri: item.image[
+                                    Math.floor(
+                                      Math.random() * item.image?.length
+                                    )
+                                  ]?.url,
+                                }}
+                                className={`object-center w-20 h-20 rounded-full`}
+                              />
+                            ) : (
+                              <Text>Not Applicable</Text>
+                            )}
+                          </Text>
+                        </DataTable.Cell>
+                        <DataTable.Cell
+                          style={{
                             width: customWidth,
                             justifyContent: "space-around",
                             alignItems: "center",
@@ -279,18 +373,31 @@ export default function () {
                           }}
                         >
                           <TouchableOpacity
-                            onPress={() => handleEditTransaction(item?._id)}
+                            onPress={() => {
+                              if (item.status !== "completed") {
+                                handleEditTransaction(item?._id);
+                              } else
+                                Alert.alert(
+                                  "Edit not allowed",
+                                  "This transaction has been completed."
+                                );
+                            }}
                           >
-                            <Feather name="edit" size={24} color="blue" />
+                            <Feather
+                              name="edit"
+                              size={24}
+                              color={
+                                item.status !== "completed" ? "blue" : "gray"
+                              }
+                            />
                           </TouchableOpacity>
+
                           <View style={{ width: 10 }} />
-                          {roles.includes("Admin") && (
-                            <TouchableOpacity
-                              onPress={() => handleDeleteTransaction(item?._id)}
-                            >
-                              <Feather name="delete" size={24} color="red" />
-                            </TouchableOpacity>
-                          )}
+                          <TouchableOpacity
+                            onPress={() => handleDeleteTransaction(item?._id)}
+                          >
+                            <Feather name="delete" size={24} color="red" />
+                          </TouchableOpacity>
                         </DataTable.Cell>
                       </DataTable.Row>
                     ))}
@@ -305,6 +412,28 @@ export default function () {
                 <Text style={{ color: textColor }}>No data available.</Text>
               </View>
             )}
+            {paginatedData?.length ? (
+              <View className={`flex items-center flex-row my-6`}>
+                <Button
+                  title="Previous"
+                  onPress={handlePrevPage}
+                  disabled={page === 0}
+                  color="#FDA7DF"
+                />
+                <Text
+                  style={{
+                    color: textColor,
+                  }}
+                  className={`px-20`}
+                >{`Page ${page + 1} of ${totalPageCount}`}</Text>
+                <Button
+                  title="Next"
+                  onPress={handleNextPage}
+                  disabled={page === totalPageCount - 1}
+                  color="#FDA7DF"
+                />
+              </View>
+            ) : null}
           </View>
         </SafeAreaView>
       )}
