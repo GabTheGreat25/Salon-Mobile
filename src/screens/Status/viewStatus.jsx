d931958b05c24af6e9f90c8a11283df948522b40
Jay200123 <kenken.ayonon@gmail.com>
2024-02-22 00:56:12 +0800

    Add view status screen

---
+++ src/screens/Status/viewStatus.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+} from "react-native";
+import { useGetScheduleByIdQuery } from "../../state/api/reducer";
+import { useNavigation } from "@react-navigation/native";
+import { LoadingScreen } from "@components";
+import { changeColor } from "@utils";
+import { BackIcon } from "@helpers";
+import { useIsFocused } from "@react-navigation/native";
+
+export default function ({ route }) {
+  const { id } = route.params;
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const { data, isLoading, refetch } = useGetScheduleByIdQuery(id);
+
+  const schedule = data?.details;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) refetch();
+    };
+    fetchData();
+  }, [isFocused]);
+
+  const { backgroundColor, textColor, colorScheme } = changeColor();
+  const borderColor =
+    colorScheme === "dark" ? "border-neutral-light" : "border-neutral-dark";
+
+  return (
+    <>
+      {isLoading ? (
+        <View
+          className={`flex-1 justify-center items-center bg-primary-default`}
+        >
+          <LoadingScreen />
+        </View>
+      ) : (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <SafeAreaView
+            style={{ backgroundColor }}
+            className={`relative flex-1`}
+          >
+            <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
+            <KeyboardAvoidingView behavior="height">
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                decelerationRate="fast"
+                scrollEventThrottle={1}
+                className={`px-6`}
+              >
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-center pb-6 text-3xl`}
+                >
+                  View Leave Details
+                </Text>
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Beautician
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={schedule?.beautician?.name}
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Leave Date
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={
+                    schedule?.date
+                      ? new Date(schedule.date).toISOString().split("T")[0]
+                      : ""
+                  }
+                />
+
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Leave Note
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={
+                    schedule?.leaveNote
+                      ? schedule?.leaveNote
+                      : "Currently Marked as Absent"
+                  }
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Leave Status
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={
+                    schedule?.leaveNoteConfirmed
+                      ? "Note Confirmed"
+                      : "Leave Note not Confirmed"
+                  }
+                />
+              </ScrollView>
+            </KeyboardAvoidingView>
+          </SafeAreaView>
+        </TouchableWithoutFeedback>
+      )}
+    </>
+  );
+}
