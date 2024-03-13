3e518610c0bcd7a7b8308b2371265633b94fc6d2
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-13 22:57:50 +0800

    Add viewExclusionById screen component

---
+++ src/screens/Exclusion/viewExclusionById.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+} from "react-native";
+import { useGetExclusionByIdQuery } from "../../state/api/reducer";
+import { useNavigation } from "@react-navigation/native";
+import { LoadingScreen } from "@components";
+import { changeColor } from "@utils";
+import { BackIcon } from "@helpers";
+import { useIsFocused } from "@react-navigation/native";
+
+export default function ({ route }) {
+  const { id } = route.params;
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const { data, isLoading, refetch } = useGetExclusionByIdQuery(id);
+
+  const exclusion = data?.details;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) refetch();
+    };
+    fetchData();
+  }, [isFocused]);
+
+  const { backgroundColor, textColor, colorScheme } = changeColor();
+  const borderColor =
+    colorScheme === "dark" ? "border-neutral-light" : "border-neutral-dark";
+
+  return (
+    <>
+      {isLoading ? (
+        <View
+          className={`flex-1 justify-center items-center bg-primary-default`}
+        >
+          <LoadingScreen />
+        </View>
+      ) : (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <SafeAreaView
+            style={{ backgroundColor }}
+            className={`relative flex-1`}
+          >
+            <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
+            <KeyboardAvoidingView behavior="height">
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                decelerationRate="fast"
+                scrollEventThrottle={1}
+                className={`px-6`}
+              >
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-center pt-12 pb-6 text-3xl`}
+                >
+                  View Exclusion Details
+                </Text>
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Chemical Name
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={exclusion?.ingredient_name}
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Chemical Category:
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={exclusion?.type.join(", ")}
+                />
+              </ScrollView>
+            </KeyboardAvoidingView>
+          </SafeAreaView>
+        </TouchableWithoutFeedback>
+      )}
+    </>
+  );
+}
