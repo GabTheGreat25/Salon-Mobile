94d5ebcc3628970e447d991c25e9640486c95248
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-14 22:01:28 +0800

    Add employee and location reducers to rootReducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -3,6 +3,8 @@ import auth from "./auth/authReducer";
 import appointment from "./appointment/appointmentReducer";
 import hiring from "./hiring/hiringReducer";
 import notification from "./notification/notificationReducer";
+import employee from "./auth/employeeReducer";
+import location from "./auth/locationReducer";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
@@ -10,5 +12,7 @@ export const rootReducer = combineReducers({
   appointment,
   hiring,
   notification,
+  employee,
+  location,
   [api.reducerPath]: api.reducer,
 });
