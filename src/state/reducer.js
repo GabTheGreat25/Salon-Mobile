496c59df951b5f8ddc51ee916e93d66c2f8fecca
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-14 15:07:38 +0800

    Add notification reducer to rootReducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth/authReducer";
 import appointment from "./appointment/appointmentReducer";
 import hiring from "./hiring/hiringReducer";
+import notification from "./notification/notificationReducer";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth,
   appointment,
   hiring,
+  notification,
   [api.reducerPath]: api.reducer,
 });
