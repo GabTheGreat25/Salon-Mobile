b83d423d0a9c3f7953a5691ab7643a84eb1abc01
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-16 01:07:15 +0800

    Add viewCustomerById screen component

---
+++ src/screens/EmployeeDashboard/viewCustomerById.jsx
@@ -0,0 +1,202 @@
+import React, { useEffect } from "react";
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+} from "react-native";
+import {
+  useGetUserByIdQuery,
+  useGetExclusionsQuery,
+} from "../../state/api/reducer";
+import { useNavigation } from "@react-navigation/native";
+import { LoadingScreen } from "@components";
+import { changeColor } from "@utils";
+import { BackIcon } from "@helpers";
+import { useIsFocused } from "@react-navigation/native";
+
+export default function ({ route }) {
+  const { id } = route.params;
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const { data, isLoading, refetch } = useGetUserByIdQuery(id);
+  const user = data?.details;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) refetch();
+    };
+    fetchData();
+  }, [isFocused]);
+
+  const { backgroundColor, textColor, colorScheme } = changeColor();
+  const borderColor =
+    colorScheme === "dark" ? "border-neutral-light" : "border-neutral-dark";
+
+  const { data: allergies, isLoading: exclusionLoading } =
+    useGetExclusionsQuery();
+  const exclusions = allergies?.details;
+
+  let filteredExclusions = exclusions
+    ?.filter((exclusion) => user?.information?.allergy?.includes(exclusion._id))
+    .map((exclusion) => exclusion.ingredient_name);
+
+  if (
+    Array.isArray(user?.information?.allergy) &&
+    (user?.information?.allergy.includes("None") ||
+      user?.information?.allergy.includes("Others"))
+  ) {
+    filteredExclusions = user?.information?.allergy;
+  }
+
+  const othersMessage =
+    Array.isArray(user?.information?.allergy) &&
+    user?.information?.allergy.includes("Others")
+      ? user?.information?.othersMessage
+      : null;
+
+  return (
+    <>
+      {isLoading || exclusionLoading ? (
+        <View
+          className={`flex-1 justify-center items-center bg-primary-default`}
+        >
+          <LoadingScreen />
+        </View>
+      ) : (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <SafeAreaView
+            style={{ backgroundColor }}
+            className={`relative flex-1`}
+          >
+            <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
+            <KeyboardAvoidingView behavior="height">
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                decelerationRate="fast"
+                scrollEventThrottle={1}
+                className={`px-6`}
+              >
+                <View className="items-center justify-center py-12">
+                  <Image
+                    key={
+                      data?.details?.image[
+                        Math.floor(Math.random() * data?.details?.image?.length)
+                      ]?.public_id
+                    }
+                    source={{
+                      uri: data?.details?.image[
+                        Math.floor(Math.random() * data?.details?.image?.length)
+                      ]?.url,
+                    }}
+                    className={`rounded-full w-60 h-60`}
+                    resizeMode="cover"
+                  />
+                </View>
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-center pb-6 text-3xl`}
+                >
+                  Customer Information
+                </Text>
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  User Name
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={user?.name}
+                  editable={false}
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Age
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={user?.age.toString()}
+                  editable={false}
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Email
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={user?.email}
+                  editable={false}
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Mobile Number
+                </Text>
+                <TextInput
+                  style={{ color: textColor }}
+                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  value={user?.contact_number}
+                  editable={false}
+                />
+                <Text
+                  style={{ color: textColor }}
+                  className={`font-semibold text-base`}
+                >
+                  Chemical Solution Exclusions
+                </Text>
+                <TextInput
+                  style={{
+                    color: textColor,
+                    height: 100,
+                    textAlignVertical: "top",
+                  }}
+                  className={`border-[1.5px] py-2 px-4 text-lg font-normal rounded-lg my-2 ${borderColor}`}
+                  autoCapitalize="none"
+                  multiline={true}
+                  value={filteredExclusions.join(", ")}
+                  editable={false}
+                />
+                {othersMessage && (
+                  <React.Fragment>
+                    <Text
+                      style={{ color: textColor }}
+                      className={`font-semibold text-base`}
+                    >
+                      Type
+                    </Text>
+                    <TextInput
+                      style={{ color: textColor }}
+                      className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                      autoCapitalize="none"
+                      value={othersMessage}
+                      editable={false}
+                    />
+                  </React.Fragment>
+                )}
+              </ScrollView>
+            </KeyboardAvoidingView>
+          </SafeAreaView>
+        </TouchableWithoutFeedback>
+      )}
+    </>
+  );
+}
