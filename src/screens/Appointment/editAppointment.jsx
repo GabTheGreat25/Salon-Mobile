3dd74084748f6efee2fb447b116daa7c1f45fa18
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-16 19:18:02 +0800

    Refactor editAppointment

--- src/screens/Appointment/editAppointment.jsx
+++ src/screens/Appointment/editAppointment.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import {
   View,
   SafeAreaView,
@@ -158,171 +158,178 @@ export default function ({ route }) {
           <LoadingScreen />
         </View>
       ) : (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <SafeAreaView
-            style={{ backgroundColor }}
-            className={`relative flex-1`}
-          >
-            <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
-            <KeyboardAvoidingView behavior="height">
-              <ScrollView
-                showsVerticalScrollIndicator={false}
-                decelerationRate="fast"
-                scrollEventThrottle={1}
-                className={`pt-10 px-6`}
-              >
-                <Text
-                  style={{ color: textColor }}
-                  className={`font-semibold text-center pt-6 text-3xl`}
+        <>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView
+              style={{ backgroundColor }}
+              className={`relative flex-1 pt-12`}
+            >
+              <BackIcon
+                navigateBack={navigation.goBack}
+                textColor={textColor}
+              />
+              <KeyboardAvoidingView behavior="height">
+                <ScrollView
+                  showsVerticalScrollIndicator={false}
+                  decelerationRate="fast"
+                  scrollEventThrottle={1}
+                  className={`pt-10 px-6`}
                 >
-                  Update Appointment Details
-                </Text>
-                <Text
-                  style={{ color: textColor }}
-                  className={`font-semibold text-base`}
-                >
-                  Price
-                </Text>
-                <TextInput
-                  style={{ color: textColor }}
-                  className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
-                  placeholder="Enter the price"
-                  placeholderTextColor={textColor}
-                  keyboardType="numeric"
-                  onChangeText={(value) =>
-                    formik.handleChange("price")(value.toString())
-                  }
-                  onBlur={formik.handleBlur("price")}
-                  value={formik.values.price.toFixed(0).toString()}
-                />
-                {formik.touched.price && formik.errors.price && (
-                  <Text style={{ color: "red" }}>{formik.errors.price}</Text>
-                )}
+                  <Text
+                    style={{ color: textColor }}
+                    className={`font-semibold text-center pt-6 text-3xl`}
+                  >
+                    Update Appointment Details
+                  </Text>
+                  <Text
+                    style={{ color: textColor }}
+                    className={`font-semibold text-base`}
+                  >
+                    Price
+                  </Text>
+                  <TextInput
+                    style={{ color: textColor }}
+                    className={`border-[1.5px] py-2 pl-4 text-lg font-normal rounded-full my-2 ${borderColor}`}
+                    placeholder="Enter the price"
+                    placeholderTextColor={textColor}
+                    keyboardType="numeric"
+                    onChangeText={(value) =>
+                      formik.handleChange("price")(value.toString())
+                    }
+                    onBlur={formik.handleBlur("price")}
+                    value={formik.values.price.toFixed(0).toString()}
+                  />
+                  {formik.touched.price && formik.errors.price && (
+                    <Text style={{ color: "red" }}>{formik.errors.price}</Text>
+                  )}
 
-                <Text
-                  style={{ color: textColor }}
-                  className="text-2xl font-semibold"
-                >
-                  Services
-                </Text>
-                <View className="flex flex-row flex-wrap justify-start gap-x-4">
-                  {services?.details?.map((service) => (
-                    <TouchableOpacity
-                      key={service._id}
-                      onPress={() => handleServicePress(service._id)}
-                      disabled={!areOptionsEmpty(service._id)}
-                      className="flex-row py-2 gap-x-2"
-                    >
-                      <View
-                        style={{
-                          height: 30,
-                          width: 30,
-                          borderColor: textColor,
-                          backgroundColor: backgroundColor,
-                        }}
-                        className="flex-row items-center justify-center border-2 rounded"
+                  <Text
+                    style={{ color: textColor }}
+                    className="text-2xl font-semibold"
+                  >
+                    Services
+                  </Text>
+                  <View className="flex flex-row flex-wrap justify-start gap-x-4">
+                    {services?.details?.map((service) => (
+                      <TouchableOpacity
+                        key={service._id}
+                        onPress={() => handleServicePress(service._id)}
+                        disabled={!areOptionsEmpty(service._id)}
+                        className="flex-row py-2 gap-x-2"
                       >
-                        {formik.values.service.includes(service._id) ? (
+                        <View
+                          style={{
+                            height: 30,
+                            width: 30,
+                            borderColor: textColor,
+                            backgroundColor: backgroundColor,
+                          }}
+                          className="flex-row items-center justify-center border-2 rounded"
+                        >
+                          {formik.values.service.includes(service._id) ? (
+                            <Text
+                              style={{ color: textColor }}
+                              className="text-lg"
+                            >
+                              ✓
+                            </Text>
+                          ) : null}
+                        </View>
+                        <View>
                           <Text
                             style={{ color: textColor }}
-                            className="text-lg"
+                            className="text-lg font-semibold"
                           >
-                            ✓
+                            {service.service_name}
                           </Text>
-                        ) : null}
-                      </View>
-                      <View>
-                        <Text
-                          style={{ color: textColor }}
-                          className="text-lg font-semibold"
-                        >
-                          {service.service_name}
-                        </Text>
-                      </View>
-                    </TouchableOpacity>
-                  ))}
-                </View>
+                        </View>
+                      </TouchableOpacity>
+                    ))}
+                  </View>
 
-                <View className="flex flex-row flex-wrap justify-start gap-x-2">
-                  {formik.values.service?.map((selectedServiceId) => {
-                    const selectedService = services?.details?.find(
-                      (service) => service._id === selectedServiceId
-                    );
+                  <View className="flex flex-row flex-wrap justify-start gap-x-2">
+                    {formik.values.service?.map((selectedServiceId) => {
+                      const selectedService = services?.details?.find(
+                        (service) => service._id === selectedServiceId
+                      );
 
-                    if (!selectedService) return null;
+                      if (!selectedService) return null;
 
-                    const serviceOptions = options?.filter((option) =>
-                      option.service.some((s) => s._id === selectedServiceId)
-                    );
+                      const serviceOptions = options?.filter((option) =>
+                        option.service.some((s) => s._id === selectedServiceId)
+                      );
 
-                    return (
-                      <View key={selectedService._id}>
-                        <Text className="my-2 text-lg font-semibold text-light-default dark:text-dark-default">
-                          Add Ons for {selectedService.service_name}
-                        </Text>
-                        {serviceOptions?.map((option) => (
-                          <TouchableOpacity
-                            key={option._id}
-                            onPress={() => handleOptionPress(option._id)}
-                            className="flex-row items-center mb-2"
-                          >
-                            <View
-                              style={{
-                                height: 30,
-                                width: 30,
-                                borderColor: textColor,
-                                backgroundColor: backgroundColor,
-                              }}
-                              className="flex-row items-center justify-center border-2 rounded"
+                      return (
+                        <View key={selectedService._id}>
+                          <Text className="my-2 text-lg font-semibold text-light-default dark:text-dark-default">
+                            Add Ons for {selectedService.service_name}
+                          </Text>
+                          {serviceOptions?.map((option) => (
+                            <TouchableOpacity
+                              key={option._id}
+                              onPress={() => handleOptionPress(option._id)}
+                              className="flex-row items-center mb-2"
                             >
-                              {formik.values.options.includes(option._id) ? (
+                              <View
+                                style={{
+                                  height: 30,
+                                  width: 30,
+                                  borderColor: textColor,
+                                  backgroundColor: backgroundColor,
+                                }}
+                                className="flex-row items-center justify-center border-2 rounded"
+                              >
+                                {formik.values.options.includes(option._id) ? (
+                                  <Text
+                                    style={{ color: textColor }}
+                                    className="text-lg"
+                                  >
+                                    ✓
+                                  </Text>
+                                ) : null}
+                              </View>
+                              <View className="flex items-center pl-2">
                                 <Text
                                   style={{ color: textColor }}
-                                  className="text-lg"
+                                  className="text-lg font-semibold"
                                 >
-                                  ✓
+                                  {option.option_name} - ₱{option.extraFee}
                                 </Text>
-                              ) : null}
-                            </View>
-                            <View className="flex items-center pl-2">
-                              <Text
-                                style={{ color: textColor }}
-                                className="text-lg font-semibold"
-                              >
-                                {option.option_name} - ₱{option.extraFee}
-                              </Text>
-                            </View>
-                          </TouchableOpacity>
-                        ))}
-                      </View>
-                    );
-                  })}
-                </View>
+                              </View>
+                            </TouchableOpacity>
+                          ))}
+                        </View>
+                      );
+                    })}
+                  </View>
 
-                <View className={`mb-20 items-center justify-center flex-col`}>
-                  <TouchableOpacity
-                    onPress={formik.handleSubmit}
-                    disabled={!formik.isValid}
+                  <View
+                    className={`mb-20 items-center justify-center flex-col`}
                   >
-                    <View className={`mb-2 flex justify-center items-center`}>
-                      <View
-                        className={`py-2 rounded-lg bg-primary-accent w-[175px]
+                    <TouchableOpacity
+                      onPress={formik.handleSubmit}
+                      disabled={!formik.isValid}
+                    >
+                      <View className={`mb-2 flex justify-center items-center`}>
+                        <View
+                          className={`py-2 rounded-lg bg-primary-accent w-[175px]
                           } ${!formik.isValid ? "opacity-50" : "opacity-100"}`}
-                      >
-                        <Text
-                          className={`font-semibold text-center text-lg`}
-                          style={{ color: textColor }}
                         >
-                          Submit
-                        </Text>
+                          <Text
+                            className={`font-semibold text-center text-lg`}
+                            style={{ color: textColor }}
+                          >
+                            Submit
+                          </Text>
+                        </View>
                       </View>
-                    </View>
-                  </TouchableOpacity>
-                </View>
-              </ScrollView>
-            </KeyboardAvoidingView>
-          </SafeAreaView>
-        </TouchableWithoutFeedback>
+                    </TouchableOpacity>
+                  </View>
+                </ScrollView>
+              </KeyboardAvoidingView>
+            </SafeAreaView>
+          </TouchableWithoutFeedback>
+        </>
       )}
     </>
   );
