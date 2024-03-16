9cc81dec47fe9c338c7303cc03d4eb86722b0bee
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-16 19:21:15 +0800

    Refactor editLeaveDate

--- src/screens/EmployeeDashboard/editLeaveDate.jsx
+++ src/screens/EmployeeDashboard/editLeaveDate.jsx
@@ -150,108 +150,115 @@ export default function ({ route }) {
           <LoadingScreen />
         </View>
       ) : (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <SafeAreaView
-            style={{ backgroundColor }}
-            className={`relative flex-1`}
-          >
-            <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
-            <ScrollView
-              showsVerticalScrollIndicator={false}
-              decelerationRate="fast"
-              scrollEventThrottle={1}
-              style={{
-                backgroundColor,
-              }}
-              className={`px-3 flex-col flex-1 pt-3`}
+        <>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView
+              style={{ backgroundColor }}
+              className={`relative flex-1`}
             >
-              <View>
+              <BackIcon
+                navigateBack={navigation.goBack}
+                textColor={textColor}
+              />
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                decelerationRate="fast"
+                scrollEventThrottle={1}
+                style={{
+                  backgroundColor,
+                }}
+                className={`px-3 flex-col flex-1 pt-3`}
+              >
+                <View>
+                  <Text
+                    style={{ color: textColor }}
+                    className={`text-2xl text-center pb-4 font-semibold`}
+                  >
+                    Select Leave Date
+                  </Text>
+                  <Calendar
+                    style={{
+                      backgroundColor: invertBackgroundColor,
+                    }}
+                    className={`mx-1 px-4 py-4 mb-2 rounded`}
+                    onDayPress={handleDayPress}
+                    markedDates={markedDates}
+                    markingType={"simple"}
+                    theme={{
+                      calendarBackground: "invertBackgroundColor",
+                      monthTextColor: "black",
+                      textSectionTitleColor: "black",
+                      todayTextColor: "#BE2EDD",
+                      arrowColor: "black",
+                    }}
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    hideExtraDays={true}
+                    hideArrows={hideArrows ? true : false}
+                    horizontal={true}
+                    pagingEnabled={true}
+                  />
+                </View>
+
                 <Text
                   style={{ color: textColor }}
-                  className={`text-2xl text-center pb-4 font-semibold`}
+                  className={`text-2xl text-center font-semibold`}
                 >
-                  Select Leave Date
+                  Leave Note
                 </Text>
-                <Calendar
-                  style={{
-                    backgroundColor: invertBackgroundColor,
-                  }}
-                  className={`mx-1 px-4 py-4 mb-2 rounded`}
-                  onDayPress={handleDayPress}
-                  markedDates={markedDates}
-                  markingType={"simple"}
-                  theme={{
-                    calendarBackground: "invertBackgroundColor",
-                    monthTextColor: "black",
-                    textSectionTitleColor: "black",
-                    todayTextColor: "#BE2EDD",
-                    arrowColor: "black",
-                  }}
-                  minDate={minDate}
-                  maxDate={maxDate}
-                  hideExtraDays={true}
-                  hideArrows={hideArrows ? true : false}
-                  horizontal={true}
-                  pagingEnabled={true}
-                />
-              </View>
-
-              <Text
-                style={{ color: textColor }}
-                className={`text-2xl text-center font-semibold`}
-              >
-                Leave Note
-              </Text>
-              {formik.touched.leaveNote && formik.errors.leaveNote && (
-                <Text style={{ color: "red" }}>{formik.errors.leaveNote}</Text>
-              )}
-              <TextInput
-                style={{
-                  color: textColor,
-                  height: 100,
-                  textAlignVertical: "top",
-                }}
-                className={`border-[1.5px] pt-2 px-4 text-lg font-normal rounded-lg mt-2 mb-6 ${borderColor}`}
-                placeholder="Add LeaveNote Here..."
-                placeholderTextColor={textColor}
-                autoCapitalize="none"
-                multiline={true}
-                onChangeText={formik.handleChange("leaveNote")}
-                onBlur={formik.handleBlur("leaveNote")}
-                value={formik.values.leaveNote}
-              />
-            </ScrollView>
-            <View
-              style={{
-                backgroundColor,
-                height: windowHeight * 0.125,
-                width: windowWidth,
-              }}
-              className={`flex-col px-10 py-5`}
-            >
-              <TouchableOpacity
-                onPress={formik.handleSubmit}
-                disabled={!formik.isValid}
-              >
-                <View
-                  style={{
-                    backgroundColor: invertBackgroundColor,
-                  }}
-                  className={`justify-center items-center rounded-md py-2`}
-                >
-                  <Text
-                    style={{ color: invertTextColor }}
-                    className={`text-center text-lg font-bold ${
-                      !formik.isValid ? "opacity-50" : "opacity-100"
-                    }`}
-                  >
-                    Confirm
+                {formik.touched.leaveNote && formik.errors.leaveNote && (
+                  <Text style={{ color: "red" }}>
+                    {formik.errors.leaveNote}
                   </Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          </SafeAreaView>
-        </TouchableWithoutFeedback>
+                )}
+                <TextInput
+                  style={{
+                    color: textColor,
+                    height: 100,
+                    textAlignVertical: "top",
+                  }}
+                  className={`border-[1.5px] pt-2 px-4 text-lg font-normal rounded-lg mt-2 mb-6 ${borderColor}`}
+                  placeholder="Add LeaveNote Here..."
+                  placeholderTextColor={textColor}
+                  autoCapitalize="none"
+                  multiline={true}
+                  onChangeText={formik.handleChange("leaveNote")}
+                  onBlur={formik.handleBlur("leaveNote")}
+                  value={formik.values.leaveNote}
+                />
+              </ScrollView>
+              <View
+                style={{
+                  backgroundColor,
+                  height: windowHeight * 0.125,
+                  width: windowWidth,
+                }}
+                className={`flex-col px-10 py-5`}
+              >
+                <TouchableOpacity
+                  onPress={formik.handleSubmit}
+                  disabled={!formik.isValid}
+                >
+                  <View
+                    style={{
+                      backgroundColor: invertBackgroundColor,
+                    }}
+                    className={`justify-center items-center rounded-md py-2`}
+                  >
+                    <Text
+                      style={{ color: invertTextColor }}
+                      className={`text-center text-lg font-bold ${
+                        !formik.isValid ? "opacity-50" : "opacity-100"
+                      }`}
+                    >
+                      Confirm
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </SafeAreaView>
+          </TouchableWithoutFeedback>
+        </>
       )}
     </>
   );
