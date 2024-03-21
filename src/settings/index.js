349f7cdf142fe48c85932852fda2ad65b1ef08cd
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-21 16:41:40 +0800

    Add CustomerTermsCondition and Waiver to settings index.js

--- src/settings/index.js
+++ src/settings/index.js
@@ -2,10 +2,14 @@ import TermsConditions from "./TermsConditions";
 import PrivacyPolicy from "./PrivacyPolicy";
 import Feedback from "./Feedback";
 import BeauticianRegisterTermsCondition from "../settings/TermsConditions/BeauticianRegisterTermsCondition";
+import CustomerTermsCondition from "./TermsConditions/CustomerTermsCondition";
+import Waiver from "./Waiver";
 
 export {
   TermsConditions,
   PrivacyPolicy,
   Feedback,
   BeauticianRegisterTermsCondition,
+  CustomerTermsCondition,
+  Waiver,
 };
