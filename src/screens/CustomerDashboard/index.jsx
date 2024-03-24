e0278af89cb264e3e80c998a2c181287859758c5
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-24 19:58:51 +0800

    Refactor code to improve performance and readability

--- src/screens/CustomerDashboard/index.jsx
+++ src/screens/CustomerDashboard/index.jsx
@@ -10,13 +10,12 @@ import {
   SafeAreaView,
   Modal,
 } from "react-native";
-import Salon from "@assets/salon-bg.png";
-import SalonService from "@assets/salon-service.png";
-import SalonHair from "@assets/salon-hair.png";
-import SalonFootSpa from "@assets/foot-spa.png";
-import SalonLipstick from "@assets/lipstick.png";
+import ServicesOne from "@assets/servicesOne.png";
+import ServicesTwo from "@assets/servicesTwo.png";
+import ServicesThree from "@assets/servicesThree.png";
+import ServicesFour from "@assets/servicesFour.png";
 import MovingSale from "@assets/moving-sale.gif";
-import { changeColor, dimensionLayout } from "@utils";
+import { changeColor } from "@utils";
 import { Feather, FontAwesome, Ionicons, AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { useGetServicesQuery } from "../../state/api/reducer";
@@ -30,7 +29,6 @@ export default function () {
   const { textColor, backgroundColor, shadowColor, colorScheme } =
     changeColor();
   const navigation = useNavigation();
-  const isDimensionLayout = dimensionLayout();
   const invertBackgroundColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
   const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
 
@@ -39,14 +37,6 @@ export default function () {
   };
 
   const { data, isLoading } = useGetServicesQuery();
-
-  const widthOne = isDimensionLayout ? windowWidth * 0.35 : windowWidth * 0.75;
-  const widthTwo = isDimensionLayout ? windowWidth * 0.25 : windowWidth * 0.55;
-  const widthThree = isDimensionLayout
-    ? windowWidth * 0.436
-    : windowWidth * 0.9536;
-  const FootSpa = isDimensionLayout ? `Organic\nFoot Spa` : "Organic Foot Spa";
-  const HairSpa = isDimensionLayout ? `Hair\nStyles` : "Hair Styles";
 
   const items = data?.details || [];
 
@@ -200,27 +190,23 @@ export default function () {
                   style={{
                     shadowColor,
                     backgroundColor: invertBackgroundColor,
-                    height: windowHeight * 0.2,
-                    width: widthOne,
+                    height: windowHeight * 0.23,
+                    width: windowWidth * 0.45,
                   }}
-                  className={`rounded flex-col shadow-2xl ${
-                    isDimensionLayout ? "mx-1" : "mx-3"
-                  }`}
+                  className={`rounded flex-col shadow-2xl mx-1`}
                 >
-                  <View className={`justify-end items-center h-1/2 pt-5`}>
+                  <View className={`justify-start items-start h-1/2 pt-5 px-4`}>
                     <Text
                       style={{ color: invertTextColor }}
-                      className={`flex-1 ${
-                        isDimensionLayout ? "text-base" : "text-2xl"
-                      } p-1 font-semibold`}
+                      className={`flex-1 text-lg p-1 font-semibold`}
                     >
-                      Pick our best offers!
+                      {`Pick our\nbest offers!`}
                     </Text>
                   </View>
                   <View className={`justify-end items-center h-1/2`}>
                     <Image
-                      source={Salon}
-                      resizeMode={isDimensionLayout ? "cover" : "stretch"}
+                      source={ServicesOne}
+                      resizeMode={"cover"}
                       className={`h-full w-full`}
                     />
                   </View>
@@ -230,63 +216,25 @@ export default function () {
                   style={{
                     shadowColor,
                     backgroundColor: invertBackgroundColor,
-                    height: windowHeight * 0.2,
-                    width: widthTwo,
+                    height: windowHeight * 0.23,
+                    width: windowWidth * 0.45,
                   }}
-                  className={`rounded flex-col shadow-2xl ${
-                    isDimensionLayout ? "mx-1" : "mx-3"
-                  }`}
+                  className={`rounded flex-col shadow-2xl mx-1`}
                 >
                   <View className={`justify-end items-center h-1/2`}>
                     <Image
-                      source={SalonService}
-                      resizeMode={isDimensionLayout ? "cover" : "stretch"}
+                      source={ServicesFour}
+                      resizeMode={"cover"}
                       className={`h-full w-full`}
                     />
                   </View>
-                  <View className={`justify-end items-center h-1/2`}>
+                  <View className={`justify-center items-center h-1/2 pt-5`}>
                     <Text
                       style={{ color: invertTextColor }}
-                      className={`flex-1 ${
-                        isDimensionLayout ? "text-base" : "text-2xl"
-                      } p-[7.25px] font-semibold`}
+                      className={`flex-1 text-lg p-1 font-semibold`}
                     >
-                      High Quality Services
+                      Check Our Budget Friendly Offers!
                     </Text>
-                  </View>
-                </View>
-
-                <View
-                  style={{
-                    shadowColor,
-                    backgroundColor: invertBackgroundColor,
-                    height: windowHeight * 0.2,
-                    width: widthTwo,
-                  }}
-                  className={`rounded flex-col shadow-2xl ${
-                    isDimensionLayout ? "mx-1" : "mx-3"
-                  }`}
-                >
-                  <View
-                    className={`justify-end items-center h-1/2 ${
-                      isDimensionLayout ? "pt-5" : "pt-2"
-                    }`}
-                  >
-                    <Text
-                      style={{ color: invertTextColor }}
-                      className={`flex-1 ${
-                        isDimensionLayout ? "text-base" : "text-2xl py-5"
-                      } font-semibold`}
-                    >
-                      {HairSpa}
-                    </Text>
-                  </View>
-                  <View className={`justify-end items-center h-1/2`}>
-                    <Image
-                      source={SalonHair}
-                      resizeMode={isDimensionLayout ? "cover" : "stretch"}
-                      className={`h-full w-full`}
-                    />
                   </View>
                 </View>
               </View>
@@ -296,30 +244,24 @@ export default function () {
                   style={{
                     shadowColor,
                     backgroundColor: invertBackgroundColor,
-                    height: windowHeight * 0.1,
-                    width: widthThree,
+                    height: windowHeight * 0.12,
+                    width: windowWidth * 0.454,
                   }}
-                  className={`rounded flex-row shadow-2xl ${
-                    isDimensionLayout ? "mx-1" : "mx-3"
-                  }`}
+                  className={`rounded flex-row shadow-2xl mx-1`}
                 >
                   <View className={`flex-1`}>
                     <Image
-                      source={SalonFootSpa}
-                      resizeMode={isDimensionLayout ? "cover" : "stretch"}
+                      source={ServicesThree}
+                      resizeMode="stretch"
                       className={`h-full w-full`}
                     />
                   </View>
-                  <View className={`justify-end items-center`}>
+                  <View className={`justify-start items-start`}>
                     <Text
                       style={{ color: invertTextColor }}
-                      className={`flex-1 ${
-                        isDimensionLayout
-                          ? "text-base p-4"
-                          : "text-lg px-4 py-6"
-                      } font-semibold`}
+                      className={`text-base py-4 px-2  font-semibold`}
                     >
-                      {FootSpa}
+                      {`Check Our\nLatest Trends`}
                     </Text>
                   </View>
                 </View>
@@ -327,29 +269,23 @@ export default function () {
                   style={{
                     shadowColor,
                     backgroundColor: invertBackgroundColor,
-                    height: windowHeight * 0.1,
-                    width: widthThree,
+                    height: windowHeight * 0.12,
+                    width: windowWidth * 0.454,
                   }}
-                  className={`rounded flex-row shadow-2xl ${
-                    isDimensionLayout ? "mx-1" : "mx-3"
-                  }`}
+                  className={`rounded flex-row shadow-2xl mx-1`}
                 >
-                  <View className={`justify-end items-center`}>
+                  <View className={`justify-start items-start`}>
                     <Text
                       style={{ color: invertTextColor }}
-                      className={`flex-1 ${
-                        isDimensionLayout
-                          ? "text-base pl-2 pr-1 py-4"
-                          : "text-lg px-4 py-6"
-                      } font-semibold`}
+                      className={`text-base py-4 px-2 font-semibold`}
                     >
-                      Choose Your Beautician
+                      {`Check Our\nMost Popular`}
                     </Text>
                   </View>
                   <View className={`flex-1`}>
                     <Image
-                      source={SalonLipstick}
-                      resizeMode={isDimensionLayout ? "cover" : "stretch"}
+                      source={ServicesTwo}
+                      resizeMode="stretch"
                       className={`h-full w-full`}
                     />
                   </View>
