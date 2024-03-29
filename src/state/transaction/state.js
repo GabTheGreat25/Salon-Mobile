d6c2c2c8297dfccc072b067712e2ceb345dc22ea
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-30 02:22:28 +0800

    Add initial state for transaction data

---
+++ src/state/transaction/state.js
@@ -0,0 +1,11 @@
+export const initialState = {
+  transactionData: {
+    employee: "",
+    customer: "",
+    date: "",
+    time: [],
+    payment: "",
+    customerType: "",
+    image: [],
+  },
+};
