8d51e3e8a7a4f3c795f6ec968882b2c5aff76ce3
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-05 01:10:47 +0800

    Add ReceptionistDashboard

---
+++ src/screens/ReceptionistDashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function () {
+  return (
+    <>
+      <View className={`flex-1 justify-center items-center`}>
+        <Text>Receptionist Dashboard</Text>
+      </View>
+    </>
+  );
+}
