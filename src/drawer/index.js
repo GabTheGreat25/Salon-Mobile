320907f0d16edd61f5b41918b1fff4e564588f19
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-05 01:06:54 +0800

    Add ReceptionistDrawer to drawer index.js

--- src/drawer/index.js
+++ src/drawer/index.js
@@ -1,5 +1,6 @@
 import AdminDrawer from "./AdminDrawer";
 import EmployeeDrawer from "./EmployeeDrawer";
 import CustomerDrawer from "./CustomerDrawer";
+import ReceptionistDrawer from "./ReceptionistDrawer";
 
-export { AdminDrawer, EmployeeDrawer, CustomerDrawer };
+export { AdminDrawer, EmployeeDrawer, CustomerDrawer, ReceptionistDrawer };
