31256ff9ae5a253dd56e9d3472eb9336415bb899
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-09 01:52:22 +0800

    Refactor receptionistBudget

--- src/screens/ReceptionistDashboard/budget.jsx
+++ src/screens/ReceptionistDashboard/budget.jsx
@@ -21,9 +21,9 @@ import {
 import { LoadingScreen, Sidebar } from "@components";
 import { appointmentSlice } from "../../state/appointment/appointmentReducer";
 import { useSelector, useDispatch } from "react-redux";
+import { useIsFocused } from "@react-navigation/native";
 
 const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
 
 export default function () {
   const customer = useSelector((state) => state.customer);
@@ -32,15 +32,40 @@ export default function () {
   const route = useRoute();
   const navigation = useNavigation();
   const dispatch = useDispatch();
+  const isFocused = useIsFocused();
+
   const invertBackgroundColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
   const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
 
-  const { data: servicesData, isLoading: servicesLoading } =
-    useGetServicesQuery();
+  const {
+    data: servicesData,
+    isLoading: servicesLoading,
+    refetch,
+  } = useGetServicesQuery();
   const services = servicesData?.details || [];
 
-  const { data: commentsData, isLoading } = useGetCommentsQuery();
+  const {
+    data: commentsData,
+    isLoading,
+    refetch: refetchComments,
+  } = useGetCommentsQuery();
   const comments = commentsData?.details || [];
+
+  const {
+    data,
+    isLoading: exclusionLoading,
+    refetch: refetchExclusions,
+  } = useGetExclusionsQuery();
+  const exclusions = data?.details;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) {
+        await Promise.all([refetch(), refetchComments(), refetchExclusions()]);
+      }
+    };
+    fetchData();
+  }, [isFocused]);
 
   const allServices = services.map((service) => {
     const matchingComments = comments.filter((comment) =>
@@ -62,9 +87,6 @@ export default function () {
   });
 
   const sortedServices = allServices.sort((a, b) => a?.price - b?.price);
-
-  const { data, isLoading: exclusionLoading } = useGetExclusionsQuery();
-  const exclusions = data?.details;
 
   const filteredExclusions = exclusions
     ?.filter(
@@ -287,7 +309,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "ReceptionistRelevance"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -310,7 +332,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "ReceptionistPopular"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -333,7 +355,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "ReceptionistMostRecent"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -356,7 +378,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "ReceptionistBudget"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -410,7 +432,7 @@ export default function () {
                             }}
                             resizeMode="cover"
                             style={{
-                              height: windowHeight * 0.25,
+                              height: 200,
                               width: windowWidth * 0.9,
                               borderRadius: 20,
                             }}
@@ -500,7 +522,7 @@ export default function () {
                             }}
                             resizeMode="cover"
                             style={{
-                              height: windowHeight * 0.25,
+                              height: 200,
                               width: windowWidth * 0.9,
                               borderRadius: 20,
                             }}
