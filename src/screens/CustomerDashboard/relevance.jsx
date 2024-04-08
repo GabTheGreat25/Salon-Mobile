94462326fea69473c49542fb927fa5bde95c21d3
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-09 01:48:41 +0800

    Refactor relevance

--- src/screens/CustomerDashboard/relevance.jsx
+++ src/screens/CustomerDashboard/relevance.jsx
@@ -21,9 +21,9 @@ import {
 import { LoadingScreen, Sidebar } from "@components";
 import { appointmentSlice } from "../../state/appointment/appointmentReducer";
 import { useSelector, useDispatch } from "react-redux";
+import { useIsFocused } from "@react-navigation/native";
 
 const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
 
 export default function () {
   const auth = useSelector((state) => state.auth.user);
@@ -35,13 +35,37 @@ export default function () {
   const invertBackgroundColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
   const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
 
-  const { data, isLoading } = useGetServicesQuery();
+  const isFocused = useIsFocused();
 
-  const services = data?.details || [];
+  const {
+    data: servicesData,
+    isLoading: servicesLoading,
+    refetch,
+  } = useGetServicesQuery();
+  const services = servicesData?.details || [];
 
-  const { data: commentsData, isLoading: commentsLoading } =
-    useGetCommentsQuery();
+  const {
+    data: commentsData,
+    isLoading,
+    refetch: refetchComments,
+  } = useGetCommentsQuery();
   const comments = commentsData?.details || [];
+
+  const {
+    data,
+    isLoading: exclusionLoading,
+    refetch: refetchExclusions,
+  } = useGetExclusionsQuery();
+  const exclusions = data?.details;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) {
+        await Promise.all([refetch(), refetchComments(), refetchExclusions()]);
+      }
+    };
+    fetchData();
+  }, [isFocused]);
 
   const allServices = services.map((service) => {
     const matchingComments = comments.filter((comment) =>
@@ -61,10 +85,6 @@ export default function () {
       ratings: averageRating,
     };
   });
-
-  const { data: exclusionData, isLoading: exclusionLoading } =
-    useGetExclusionsQuery();
-  const exclusions = exclusionData?.details;
 
   const filteredExclusions = exclusions
     ?.filter(
@@ -247,7 +267,7 @@ export default function () {
 
   return (
     <>
-      {isLoading || commentsLoading || exclusionLoading ? (
+      {isLoading || servicesLoading || exclusionLoading ? (
         <View
           className={`flex-1 justify-center items-center bg-primary-default`}
         >
@@ -289,7 +309,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "Relevance"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -312,7 +332,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "Popular"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -335,7 +355,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "MostRecent"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -358,7 +378,7 @@ export default function () {
                       style={{
                         backgroundColor:
                           selectedOption === "Budget"
-                            ? "#FDA7DF"
+                            ? "#FFB6C1"
                             : invertBackgroundColor,
                       }}
                       className={`rounded-full px-4 py-2`}
@@ -409,13 +429,16 @@ export default function () {
                             }}
                             resizeMode="cover"
                             style={{
-                              height: windowHeight * 0.25,
+                              height: 200,
                               width: windowWidth * 0.9,
                               borderRadius: 20,
                             }}
                           />
-                          <TouchableOpacity onPress={() => handlePress(item)}>
-                            <View className={`absolute left-[315px] bottom-2`}>
+                          <TouchableOpacity
+                            onPress={() => handlePress(item)}
+                            className={`relative`}
+                          >
+                            <View className={`absolute left-2 bottom-2`}>
                               <Ionicons
                                 name="add-circle-sharp"
                                 size={50}
@@ -496,13 +519,16 @@ export default function () {
                             }}
                             resizeMode="cover"
                             style={{
-                              height: windowHeight * 0.25,
+                              height: 200,
                               width: windowWidth * 0.9,
                               borderRadius: 20,
                             }}
                           />
-                          <TouchableOpacity onPress={() => handlePress(item)}>
-                            <View className={`absolute left-[315px] bottom-2`}>
+                          <TouchableOpacity
+                            onPress={() => handlePress(item)}
+                            className={`relative`}
+                          >
+                            <View className={`absolute left-2 bottom-2`}>
                               <Ionicons
                                 name="add-circle-sharp"
                                 size={50}
