3b540ebed21e0dc67aa26642284c71494dc0c855
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-09 01:56:41 +0800

    Refactor Feedback

--- src/settings/Feedback/index.jsx
+++ src/settings/Feedback/index.jsx
@@ -12,14 +12,22 @@ import {
 import { changeColor } from "@utils";
 import { BackIcon } from "@helpers";
 import { useNavigation } from "@react-navigation/native";
+import { TextInputMask } from "react-native-masked-text";
 
 export default function () {
   const navigation = useNavigation();
 
-  const { textColor, backgroundColor, colorScheme } = changeColor();
+  const { textColor, backgroundColor, borderColor, colorScheme } =
+    changeColor();
 
-  const invertBackgroundColor = colorScheme === "dark" ? "#e5e5e5" : "#FDA7DF";
+  const invertBackgroundColor = colorScheme === "dark" ? "#e5e5e5" : "#FFB6C1";
   const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
+
+  const handlePhoneNumberChange = (event) => {
+    let phoneNumber = event.nativeEvent.text.replace(/[-\s]/g, "");
+    phoneNumber = phoneNumber.substring(0, 11);
+    // formik.setFieldValue("contact_number", phoneNumber);
+  };
 
   return (
     <>
@@ -33,13 +41,13 @@ export default function () {
             className={`py-12`}
           >
             <View
-              className={`justify-center rounded-md bg-white m-8 p-4`}
+              className={`justify-center rounded-lg m-8 p-4`}
               style={{
                 backgroundColor: invertBackgroundColor,
               }}
             >
               <Text
-                className={`mb-3 text-lg font-semibold`}
+                className={`mb-3 text-2xl font-semibold`}
                 style={{
                   color: invertTextColor,
                 }}
@@ -47,7 +55,7 @@ export default function () {
                 Let's Talk!
               </Text>
               <Text
-                className={`text-sm font-semibold mb-2`}
+                className={`text-lg font-semibold mb-2`}
                 style={{
                   color: invertTextColor,
                 }}
@@ -55,11 +63,13 @@ export default function () {
                 Full name
               </Text>
               <TextInput
-                className={`h-8 border-1 border-black bg-gray-100 mb-4 p-2 rounded`}
+                style={{ borderColor, backgroundColor, color: textColor }}
+                className={`h-8 border-1 mb-4 p-2 rounded-lg`}
                 placeholder="Your name"
+                placeholderTextColor={textColor}
               />
               <Text
-                className={`text-sm font-semibold mb-2`}
+                className={`text-lg font-semibold mb-2`}
                 style={{
                   color: invertTextColor,
                 }}
@@ -67,37 +77,57 @@ export default function () {
                 Email address:
               </Text>
               <TextInput
-                className={`h-8 border-1 border-black bg-gray-100 mb-4 p-2 rounded`}
+                style={{ borderColor, backgroundColor, color: textColor }}
+                className={`h-8 border-1 mb-4 p-2 rounded-lg`}
                 placeholder="Your email address"
+                placeholderTextColor={textColor}
               />
               <Text
-                className={`text-sm font-semibold mb-2`}
+                className={`text-lg font-semibold mb-2`}
                 style={{
                   color: invertTextColor,
                 }}
               >
                 Mobile Number
               </Text>
-              <TextInput
-                className={`h-8 border-1 border-black bg-gray-100 mb-4 p-2 rounded`}
-                placeholder="09XXXXXXXX"
+              <TextInputMask
+                style={{ borderColor, backgroundColor, color: textColor }}
+                type={"custom"}
+                options={{
+                  mask: "9999 - 999 - 9999",
+                }}
+                className={`h-8 border-1 mb-4 p-2 rounded-lg`}
+                placeholder="09XX - XXX - XXXX"
+                placeholderTextColor={textColor}
+                autoCapitalize="none"
+                onChange={handlePhoneNumberChange}
+                // onBlur={formik.handleBlur("contact_number")}
+                // value={formik.values.contact_number}
+                keyboardType="numeric"
               />
               <Text
-                className={`text-sm font-semibold mb-2`}
+                className={`text-lg font-semibold mb-2`}
                 style={{
                   color: invertTextColor,
                 }}
               >
-                What would you like to discuss?:
+                What would you like to discuss?
               </Text>
               <TextInput
-                className={`border-1 border-black bg-gray-100 mb-4 pt-3 rounded h-24 resize-none`}
-                placeholder="Your feedback here"
+                style={{ borderColor, backgroundColor, color: textColor }}
+                className={`h-32 resize-none border-1 mb-4 px-2 pt-2 rounded-lg`}
+                placeholder="Add Your feedback here"
                 textAlignVertical="top"
                 multiline
+                placeholderTextColor={textColor}
               />
-              <TouchableOpacity className={`rounded bg-pink-400 h-10 w-full`}>
-                <Text className={`text-center text-lg text-white mt-2.5`}>
+              <TouchableOpacity
+                className={`rounded-md bg-primary-accent w-full justify-center items-center`}
+              >
+                <Text
+                  style={{ color: textColor }}
+                  className={`text-xl font-semibold py-1`}
+                >
                   Submit
                 </Text>
               </TouchableOpacity>
