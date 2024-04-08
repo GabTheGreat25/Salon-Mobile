7e7795839ed028325823a4c88c17d5731545b390
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-09 01:44:02 +0800

    Update EmployeeDrawer component to remove unused imports and fix color scheme

--- src/drawer/EmployeeDrawer/index.jsx
+++ src/drawer/EmployeeDrawer/index.jsx
@@ -14,7 +14,7 @@ import {
   BeauticianAppointment,
   BeauticianAppointmentHistory,
 } from "@screens";
-import { changeColor, dimensionLayout } from "@utils";
+import { changeColor } from "@utils";
 import { RESOURCE } from "@constants";
 import { UserImage } from "@components";
 import { Text, TouchableOpacity, View, ScrollView } from "react-native";
@@ -28,11 +28,8 @@ export default function () {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth?.user);
   const { backgroundColor, textColor, shadowColor } = changeColor();
-  const isDimensionLayout = dimensionLayout();
   const screenWidth = Dimensions.get("window").width;
-  const viewWidth = isDimensionLayout
-    ? screenWidth * RESOURCE.NUMBER.POINT_SEVENTY_FIVE
-    : screenWidth * RESOURCE.NUMBER.POINT_FIVE;
+  const viewWidth = screenWidth * RESOURCE.NUMBER.POINT_SEVENTY_FIVE;
 
   const handleLogout = async () => {
     try {
@@ -108,11 +105,11 @@ export default function () {
         }}
         screenOptions={{
           drawerStyle: {
-            backgroundColor: backgroundColor,
+            backgroundColor,
             width: viewWidth,
           },
           headerStyle: {
-            backgroundColor: backgroundColor,
+            backgroundColor,
             shadowColor: shadowColor,
           },
           headerShown: true,
@@ -123,9 +120,9 @@ export default function () {
         <Drawer.Screen
           name="EmployeeDashboard"
           options={{
-            drawerActiveBackgroundColor: "#F78FB3",
+            drawerActiveBackgroundColor: "#FF7086",
             drawerActiveTintColor: textColor,
-            drawerInactiveTintColor: "#FDA7DF",
+            drawerInactiveTintColor: "#FFB6C1",
             drawerLabel: () => (
               <Text
                 className={`text-xl font-base`}
@@ -148,9 +145,9 @@ export default function () {
         <Drawer.Screen
           name="EditEmployeeProfile"
           options={{
-            drawerActiveBackgroundColor: "#F78FB3",
+            drawerActiveBackgroundColor: "#FF7086",
             drawerActiveTintColor: textColor,
-            drawerInactiveTintColor: "#FDA7DF",
+            drawerInactiveTintColor: "#FFB6C1",
             drawerLabel: () => (
               <Text
                 className={`text-xl font-base`}
@@ -173,9 +170,9 @@ export default function () {
         <Drawer.Screen
           name="LeaveDateBeautician"
           options={{
-            drawerActiveBackgroundColor: "#F78FB3",
+            drawerActiveBackgroundColor: "#FF7086",
             drawerActiveTintColor: textColor,
-            drawerInactiveTintColor: "#FDA7DF",
+            drawerInactiveTintColor: "#FFB6C1",
             drawerLabel: () => (
               <Text
                 className={`text-xl font-base`}
@@ -198,9 +195,9 @@ export default function () {
         <Drawer.Screen
           name="GetAllLeaveDate"
           options={{
-            drawerActiveBackgroundColor: "#F78FB3",
+            drawerActiveBackgroundColor: "#FF7086",
             drawerActiveTintColor: textColor,
-            drawerInactiveTintColor: "#FDA7DF",
+            drawerInactiveTintColor: "#FFB6C1",
             drawerLabel: () => (
               <Text
                 className={`text-xl font-base`}
@@ -223,9 +220,9 @@ export default function () {
         <Drawer.Screen
           name="BeauticianAppointment"
           options={{
-            drawerActiveBackgroundColor: "#F78FB3",
+            drawerActiveBackgroundColor: "#FF7086",
             drawerActiveTintColor: textColor,
-            drawerInactiveTintColor: "#FDA7DF",
+            drawerInactiveTintColor: "#FFB6C1",
             drawerLabel: () => (
               <Text
                 className={`text-xl font-base`}
@@ -249,9 +246,9 @@ export default function () {
         <Drawer.Screen
           name="BeauticianAppointmentHistory"
           options={{
-            drawerActiveBackgroundColor: "#F78FB3",
+            drawerActiveBackgroundColor: "#FF7086",
             drawerActiveTintColor: textColor,
-            drawerInactiveTintColor: "#FDA7DF",
+            drawerInactiveTintColor: "#FFB6C1",
             drawerLabel: () => (
               <Text
                 className={`text-xl font-base`}
