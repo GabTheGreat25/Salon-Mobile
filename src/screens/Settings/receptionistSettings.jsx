f2d832530a6dae381954b26629a7af610e5429db
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-09 01:55:09 +0800

    Add receptionistSettings

---
+++ src/screens/Settings/receptionistSettings.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { changeColor } from "@utils";
+import { useNavigation } from "@react-navigation/native";
+import { BackIcon } from "@helpers";
+
+export default function () {
+  const { textColor, backgroundColor } = changeColor();
+
+  const navigation = useNavigation();
+
+  const terms = () => {
+    navigation.navigate("TermsCondition");
+  };
+
+  const policy = () => {
+    navigation.navigate("PrivacyPolicy");
+  };
+
+  const feedback = () => {
+    navigation.navigate("Feedback");
+  };
+
+  return (
+    <>
+      <SafeAreaView style={{ backgroundColor }} className={`flex-1`}>
+        <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          decelerationRate="fast"
+          scrollEventThrottle={1}
+          style={{
+            backgroundColor,
+          }}
+          className={`pt-16 px-4`}
+        >
+          <Text
+            className={`text-2xl font-semibold pb-4`}
+            style={{
+              color: textColor,
+            }}
+          >
+            How can we help?
+          </Text>
+          <TouchableOpacity
+            className={`px-4 mb-3 flex-row justify-between items-center w-full `}
+          >
+            <Text
+              className={`text-lg font-semibold`}
+              style={{
+                color: textColor,
+              }}
+            >
+              Terms And Conditions
+            </Text>
+            <Feather name="chevron-right" size={40} color={textColor} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            className={`px-4 mb-3 flex-row justify-between items-center w-full `}
+          >
+            <Text
+              className={`text-lg font-semibold`}
+              style={{
+                color: textColor,
+              }}
+            >
+              Privacy Policy
+            </Text>
+            <Feather name="chevron-right" size={40} color={textColor} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            className={`px-4 mb-3 flex-row justify-between items-center w-full `}
+          >
+            <Text
+              className={`text-lg font-semibold`}
+              style={{
+                color: textColor,
+              }}
+            >
+              FAQ
+            </Text>
+            <Feather name="chevron-right" size={40} color={textColor} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            className={`px-4 mb-3 flex-row justify-between items-center w-full `}
+          >
+            <Text
+              className={`text-lg font-semibold`}
+              style={{
+                color: textColor,
+              }}
+            >
+              About Us
+            </Text>
+            <Feather name="chevron-right" size={40} color={textColor} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            className={`px-4 mb-3 flex-row justify-between items-center w-full `}
+          >
+            <Text
+              className={`text-lg font-semibold`}
+              style={{
+                color: textColor,
+              }}
+            >
+              Add Feedback
+            </Text>
+            <Feather name="chevron-right" size={40} color={textColor} />
+          </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+}
