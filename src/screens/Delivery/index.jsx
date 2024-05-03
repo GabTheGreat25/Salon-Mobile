a16aad9217ea78e19d04b5c037b3b222e72e860b
Jay200123 <kenken.ayonon@gmail.com>
2024-05-03 23:40:55 +0800

    Fix Delivery Table InvertTextColor

--- src/screens/Delivery/index.jsx
+++ src/screens/Delivery/index.jsx
@@ -43,7 +43,7 @@ export default function () {
   const { backgroundColor, textColor, borderColor, colorScheme } =
     changeColor();
 
-  const invertTextColor = colorScheme === "dark" ? "#212B36" : "#e5e5e5";
+  const invertTextColor = colorScheme === "dark" ? "#e5e5e5" : "#212B36";
 
   const [deleteDelivery, { isLoading: isDeleting }] =
     useDeleteDeliveryMutation();
@@ -155,7 +155,7 @@ export default function () {
                 onPress={createDelivery}
               >
                 <Text
-                  style={{ color: invertTextColor }}
+                style={{color: invertTextColor}}
                   className={`text-lg font-semibold`}
                 >
                   Create Delivery
