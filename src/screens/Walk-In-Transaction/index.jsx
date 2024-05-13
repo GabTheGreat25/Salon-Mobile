31d5b20ecd51876553fcb44f4d0c78373ac75f37
Jay200123 <kenken.ayonon@gmail.com>
2024-05-14 05:09:57 +0800

    Add walk in transaction table

---
+++ src/screens/Walk-In-Transaction/index.jsx
@@ -0,0 +1,466 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Alert,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import {
+  useGetAppointmentsQuery,
+  useDeleteAppointmentMutation,
+  useGetTransactionsQuery,
+} from "../../state/api/reducer";
+import { LoadingScreen } from "@components";
+import Toast from "react-native-toast-message";
+import { BackIcon } from "@helpers";
+import { DataTable } from "react-native-paper";
+import { Feather } from "@expo/vector-icons";
+import { changeColor } from "@utils";
+import { useNavigation } from "@react-navigation/native";
+import { saveDeletedId, getDeletedIds } from "../../helpers/DeleteItem";
+import { useIsFocused } from "@react-navigation/native";
+
+const { width: deviceWidth } = Dimensions.get("window");
+
+export default function () {
+  const isFocused = useIsFocused();
+  const navigation = useNavigation();
+
+  const customWidth = deviceWidth * 0.3;
+
+  const { data, isLoading, refetch } = useGetAppointmentsQuery();
+  const { data: transactions, refetch: refetchTransactions } =
+    useGetTransactionsQuery();
+
+  const appointments = data?.details;
+  
+  const today = new Date();
+  const walkins = appointments?.filter((a) => {
+    const appointmentDate = new Date(a?.date);
+
+    return (
+      appointmentDate.getDate() === today.getDate() &&
+      appointmentDate.getMonth() === today.getMonth() &&
+      appointmentDate.getFullYear() === today.getFullYear() ||
+      a?.transaction?.hasAppointmentFee === false
+    );
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isFocused) {
+        await Promise.all([refetch(), refetchTransactions()]);
+      }
+    };
+    fetchData();
+  }, [isFocused]);
+
+  const { backgroundColor, textColor, borderColor } = changeColor();
+
+  const completedTransactions = transactions?.details?.filter(
+    (transaction) => transaction?.status === "completed"
+  );
+
+  const completedAppointmentIds = completedTransactions?.map(
+    (transaction) => transaction.appointment._id
+  );
+
+  const [deleteAppointment, { isLoading: isDeleting }] =
+    useDeleteAppointmentMutation();
+
+  const [deletedIds, setDeletedIds] = useState([]);
+  const [page, setPage] = useState(0);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    const fetchDeletedIds = async () => {
+      const ids = await getDeletedIds();
+      setDeletedIds(ids);
+    };
+
+    fetchDeletedIds();
+  }, []);
+
+  const handleEditAppointment = (id) => {
+    if (completedAppointmentIds.includes(id)) {
+      Alert.alert("Edit not allowed", "This appointment has been completed.");
+      return;
+    }
+    navigation.navigate("EditAppointment", { id });
+  };
+
+  const handleDeleteAppointment = async (id) => {
+    Alert.alert(
+      "Delete Appointment",
+      "Are you sure you want to delete this appointment?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: async () => {
+            try {
+              const response = await deleteAppointment(id).unwrap();
+              await saveDeletedId(id);
+              setDeletedIds((prevIds) => [...prevIds, id]);
+              refetch();
+              Toast.show({
+                type: "success",
+                position: "top",
+                text1: "Appointment Successfully Deleted",
+                text2: `${response?.message}`,
+                visibilityTime: 3000,
+                autoHide: true,
+              });
+              if (paginatedData.length === 1) setPage(0);
+            } catch (error) {
+              Toast.show({
+                type: "error",
+                position: "top",
+                text1: "Error Deleting Appointment",
+                text2: `${error?.data?.error?.message}`,
+                visibilityTime: 3000,
+                autoHide: true,
+              });
+            }
+          },
+        },
+      ]
+    );
+  };
+
+  const filteredData = walkins?.filter((item) => !deletedIds.includes(item?._id)).sort((a, b) => new Date(a.date) - new Date(b.date)) || [];
+
+  const totalPageCount = Math.ceil(filteredData.length / itemsPerPage);
+  const paginatedData = filteredData.slice(
+    page * itemsPerPage,
+    (page + 1) * itemsPerPage
+  );
+
+  const handleNextPage = () => {
+    if (page < totalPageCount - 1) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleViewAppointment = (id) => {
+    navigation.navigate("ViewAppointment", { id });
+  };
+
+  return (
+    <>
+      {isLoading || isDeleting ? (
+        <View
+          className={`flex-1 justify-center items-center bg-primary-default`}
+        >
+          <LoadingScreen />
+        </View>
+      ) : (
+        <>
+          <SafeAreaView
+            style={{ backgroundColor }}
+            className={`relative flex-1`}
+          >
+            <BackIcon navigateBack={navigation.goBack} textColor={textColor} />
+            <View className={`flex-1 items-center justify-center pt-10`}>
+              {paginatedData?.length ? (
+                <ScrollView
+                  style={{ backgroundColor }}
+                  showsVerticalScrollIndicator={false}
+                >
+                  <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                    <DataTable>
+                      <DataTable.Header
+                        style={{
+                          backgroundColor,
+                          borderBottomWidth: 1,
+                          borderBottomColor: borderColor,
+                        }}
+                      >
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>
+                            Appointment Day
+                          </Text>
+                        </DataTable.Title>
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>Price</Text>
+                        </DataTable.Title>
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>Service Name</Text>
+                        </DataTable.Title>
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>Add Ons</Text>
+                        </DataTable.Title>
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>Beautician</Text>
+                        </DataTable.Title>
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>Customer</Text>
+                        </DataTable.Title>
+                        <DataTable.Title
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: 10,
+                            width: customWidth,
+                          }}
+                        >
+                          <Text style={{ color: textColor }}>Actions</Text>
+                        </DataTable.Title>
+                      </DataTable.Header>
+                      {paginatedData?.map((item) => (
+                        <DataTable.Row
+                          key={item?._id}
+                          style={{
+                            backgroundColor,
+                            borderBottomWidth: 1,
+                            borderBottomColor: borderColor,
+                          }}
+                        >
+                          <DataTable.Cell
+                            style={{
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 10,
+                              width: customWidth,
+                            }}
+                          >
+                            <Text
+                              style={{ color: textColor }}
+                              numberOfLines={1}
+                              ellipsizeMode="tail"
+                            >
+                              {item.date && item.time
+                                ? item.time.length === 1
+                                  ? `${
+                                      new Date(item.date)
+                                        .toISOString()
+                                        .split("T")[0]
+                                    } ${item.time[0]}`
+                                  : `${
+                                      new Date(item.date)
+                                        .toISOString()
+                                        .split("T")[0]
+                                    } ${item.time[0]} to ${
+                                      item.time[item.time.length - 1]
+                                    }`
+                                : ""}
+                            </Text>
+                          </DataTable.Cell>
+                          <DataTable.Cell
+                            style={{
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 10,
+                              width: customWidth,
+                            }}
+                          >
+                            <Text
+                              style={{ color: textColor }}
+                              numberOfLines={1}
+                              ellipsizeMode="tail"
+                            >
+                              ₱{item?.price.toFixed(0)}
+                            </Text>
+                          </DataTable.Cell>
+                          <DataTable.Cell
+                            style={{
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 10,
+                              width: customWidth,
+                            }}
+                          >
+                            <Text
+                              style={{ color: textColor }}
+                              numberOfLines={1}
+                              ellipsizeMode="tail"
+                            >
+                              {Array.isArray(item?.service)
+                                ? item?.service
+                                    ?.map((item) => item?.service_name)
+                                    .join(", ")
+                                : item?.service?.service_name}
+                            </Text>
+                          </DataTable.Cell>
+                          <DataTable.Cell
+                            style={{
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 10,
+                              width: customWidth,
+                            }}
+                          >
+                            <Text
+                              style={{ color: textColor }}
+                              numberOfLines={1}
+                              ellipsizeMode="tail"
+                            >
+                              {Array.isArray(item?.option)
+                                ? item?.option
+                                    ?.map((item) => item?.option_name)
+                                    .join(", ") || "None"
+                                : item?.option?.option_name || "None"}
+                            </Text>
+                          </DataTable.Cell>
+                          <DataTable.Cell
+                            style={{
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 10,
+                              width: customWidth,
+                            }}
+                          >
+                            <Text
+                              style={{ color: textColor }}
+                              numberOfLines={1}
+                              ellipsizeMode="tail"
+                            >
+                              {Array.isArray(item?.beautician)
+                                ? item?.beautician
+                                    ?.map((item) => item?.name)
+                                    .join(", ")
+                                : item?.beautician?.name}
+                            </Text>
+                          </DataTable.Cell>
+                          <DataTable.Cell
+                            style={{
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 10,
+                              width: customWidth,
+                            }}
+                          >
+                            <Text
+                              style={{ color: textColor }}
+                              numberOfLines={1}
+                              ellipsizeMode="tail"
+                            >
+                              {item?.customer?.name}
+                            </Text>
+                          </DataTable.Cell>
+                          <DataTable.Cell
+                            style={{
+                              width: customWidth,
+                              justifyContent: "space-around",
+                              alignItems: "center",
+                              padding: 10,
+                            }}
+                          >
+                            <TouchableOpacity
+                              onPress={() => handleViewAppointment(item?._id)}
+                            >
+                              {/* <TouchableOpacity
+                            onPress={() => handleEditAppointment(item?._id)}
+                          >
+                            <Feather name="edit" size={24} color="blue" />
+                          </TouchableOpacity>
+                          <View style={{ width: 10 }} /> */}
+
+                              <Feather name="eye" size={24} color="green" />
+                            </TouchableOpacity>
+
+                            <View style={{ width: 10 }} />
+
+                            <TouchableOpacity
+                              onPress={() => handleDeleteAppointment(item?._id)}
+                            >
+                              <Feather name="delete" size={24} color="red" />
+                            </TouchableOpacity>
+                          </DataTable.Cell>
+                        </DataTable.Row>
+                      ))}
+                    </DataTable>
+                  </ScrollView>
+                </ScrollView>
+              ) : (
+                <View
+                  className={`flex-1 justify-center items-center`}
+                  style={{ backgroundColor }}
+                >
+                  <Text style={{ color: textColor }}>No data available.</Text>
+                </View>
+              )}
+              {paginatedData?.length ? (
+                <View className={`flex items-center flex-row my-6`}>
+                  <Button
+                    title="Previous"
+                    onPress={handlePrevPage}
+                    disabled={page === 0}
+                    color="#FFB6C1"
+                  />
+                  <Text
+                    style={{
+                      color: textColor,
+                    }}
+                    className={`px-20`}
+                  >{`Page ${page + 1} of ${totalPageCount}`}</Text>
+                  <Button
+                    title="Next"
+                    onPress={handleNextPage}
+                    disabled={page === totalPageCount - 1}
+                    color="#FFB6C1"
+                  />
+                </View>
+              ) : null}
+            </View>
+          </SafeAreaView>
+        </>
+      )}
+    </>
+  );
+}
